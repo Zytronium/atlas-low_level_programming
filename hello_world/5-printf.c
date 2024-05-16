29d014b627a965de2a59aa0d5cc1007cbb5fb24c
root <dastelljes@outlook.com>
2024-05-16 20:40:20 +0000

    commit msg

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -6,5 +6,5 @@
 int main(void) /*prints a string with printf and returns with zero*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0); /*returns 0*/
+	return (0); /*return - returns 0*/
 }
