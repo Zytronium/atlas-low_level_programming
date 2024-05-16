ece39dfba7e3388ac8e81a2b904be43da293ed59
root <dastelljes@outlook.com>
2024-05-16 20:21:01 +0000

    commit msg

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*prints a string with puts and returns with int 0*/
-/*prints a string with puts & returns with zero*/int main(void) /*prints string & returns 0 0*/
+/*prints string & returns w/ 0*/int main(void) /*prints string & returns0*/
 { /*prints a string with puts and returns with int 0*/
 	puts("\"Programming is like building a multilingual puzzle");
 
