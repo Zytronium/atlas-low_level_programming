7eea39159ee6e71e5bbf16ca6f052d03948033c7
root <dastelljes@outlook.com>
2024-05-16 21:07:11 +0000

    commit msg

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,6 +6,16 @@
  */
 int main(void)
 {
-	printf("Size of a char: ${sizeof(char)} byte(s)");
+	char character;
+	int integer;
+	long int long_int;
+	long long int longer_int;
+	float floating;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(character));
+	printf("Size of an int: %d byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long_int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(longer_int));
+	printf("Size of a float: %f byte(s)\n", sizeof(floating));
 	return (0);
 }
