cb59d73428b1278cb75dec9bd8c7aa87b089dc40
root <dastelljes@outlook.com>
2024-05-17 23:51:10 +0000

    commit msg

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints every letter in the alphabet excerpt e and q, and then prints a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l2 != 'q' && l2 != 'e')
+			putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
