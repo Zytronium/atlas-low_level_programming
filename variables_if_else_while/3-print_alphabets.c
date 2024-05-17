792d95dac94ae230addbce48b2023d376770af13
root <dastelljes@outlook.com>
2024-05-17 23:47:02 +0000

    commit msg

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints every letter in the alphabet lowercase,
+ * then again uppercase, and then a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+	char l2;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	for (l2 = 'A'; l2 <= 'Z'; l2++)
+	{
+		putchar(l2);
+	}
+	putchar('\n');
+
+	return (0);
+}
