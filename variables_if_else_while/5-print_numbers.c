94a8f97cce648f23d9d5c1f246307ddbaaa038d4
root <dastelljes@outlook.com>
2024-05-17 23:58:38 +0000

    commit msg

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints every single-digit number
+ * of base 10 starting from 0, then a new line,
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
