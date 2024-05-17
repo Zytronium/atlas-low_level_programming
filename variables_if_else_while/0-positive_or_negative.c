8cd45d74e85a6b7d6704298a8889a9a486c582da
root <dastelljes@outlook.com>
2024-05-17 21:57:08 +0000

    commit msg

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,6 +12,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf(n);
-        printf(if (n > 0) "is positive\n" else if (n < 0) "is negative\n" else "is zero\n");
+        printf("%d %s\n", n, (n > 0) ? "is positive" : (n < 0) ? "is negative" : "is zero");
         return (0);
 }
