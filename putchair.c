1e0421dd1b9f55d15eea5b461144259df8334ea0
root <dastelljes@outlook.com>
2024-05-17 17:59:08 +0000

    commit msg

---
+++ putchair.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('i');
+        putchar('r');
+	putchar('\n');
+
+        return (0);
+}
