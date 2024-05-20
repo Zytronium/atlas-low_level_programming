dc3daad2b524c6dfdf076db092fec5d8024de975
root <dastelljes@outlook.com>
2024-05-20 00:12:37 +0000

    commit msg

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description:  ashdfashdf
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while(c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	return(0);
+}
