a8eadcd654cc25afffde88d764af3d118c676864
root <dastelljes@outlook.com>
2024-05-20 01:34:56 +0000

    commit msg

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
+ * _isalpha - checks for letters
  * @c: ASCII code for provided character
  *
  * Return: 1 or 0
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
+	else if (c >=65 && c <=90)
+		return (1);
 	else
 		return (0);
 }
