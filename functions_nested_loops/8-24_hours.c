b23fc5bbfcdadc22f0c6e2517fa5a9ea37f38ca6
root <dastelljes@outlook.com>
2024-05-20 04:20:05 +0000

    commit msg

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - clock clock clock clock tick tock
+ *
+ * Return: the last digit of n
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	hour = 0;
+	minute = 0;
+
+	while (hour <= 12)
+	{
+		if (hour < 10)
+			_putchar('0');
+		_putchar('0' + hour);
+		hour++;
+		while (minute < 60)
+		{
+			if (minute < 10)
+				_putchar('0');
+			_putchar('0' + minute);
+			minute++;
+		}
+		minute = 0;
+	}
+
+}
