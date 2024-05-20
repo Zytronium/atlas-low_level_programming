b142fe12243fbdd33ca5ec5bf59beb65bb7dfeb1
root <dastelljes@outlook.com>
2024-05-20 00:30:57 +0000

    commit msg

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * print_alphabet - entry point I think
  * Description: prints the alphabet, in lowercase, followed by a new line.
  * Return: 0
  */
