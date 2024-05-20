1bebd9ec7a958f37d3d173ae08c1f7745fe37537
root <dastelljes@outlook.com>
2024-05-20 00:34:56 +0000

    commit msg

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point I think
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	i = 1;
+
+	while (i<=10)
+	{
+		print_alphabet();
+		i++
+	}
+}
