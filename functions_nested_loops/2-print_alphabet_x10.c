7a2bd833fce0449fe6ed2a73bd30652f772210a8
root <dastelljes@outlook.com>
2024-05-20 00:38:46 +0000

    commit msg

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,20 @@
 
 void print_alphabet_x10(void)
 {
+	char c;
 	int i;
 
 	i = 1;
 
 	while (i<=10)
 	{
-		print_alphabet();
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
 		i++;
-	}
 }
