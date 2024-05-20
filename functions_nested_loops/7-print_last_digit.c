806fe319d66fa232c6a28c86ca2e5017e813bfd9
root <dastelljes@outlook.com>
2024-05-20 01:52:55 +0000

    commit msg

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints a number's last digit
+ * @n: input number
+ * Return: the last digit of n
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
