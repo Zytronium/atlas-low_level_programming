3992691ec32cba70f679da6e83c3fd7eeb38db10
root <dastelljes@outlook.com>
2024-05-21 03:16:52 +0000

    commit msg

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void countDigits(int x);
+int countDigits(int x);
 void printInt(int n);
 void print_to_98(int n);
 
