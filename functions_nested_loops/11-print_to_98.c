d478a9b50464c2692f02ff4b514ad9471ecec3f7
root <dastelljes@outlook.com>
2024-05-21 03:05:56 +0000

    commit msg

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,37 +2,19 @@
 /**
  * print_to_98 - counts up or down starting from n until 98
  * @n: starting number
+ * printInt - prints an integer up to 3 digits
+ * @n: number to print
+ * countDigits - returns a number's number of digits
+ * @x: the number to count the digits of
  * Return: doesn't return
  */
 void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		if (n < 0)
-			_putchar('-');
-		if (n >= 10)
-		{
-			_putchar('0' + ((n - (n % 10)) / 10));
-			_putchar('0' + (n % 10));
-		}
-		else if (n <= -10)
-		{
-			n *= -1;
-			_putchar('0' + ((n - (n % 10)) / 10));
-			_putchar('0' + (n % 10));
-			n *= -1;
-		}
-		else if (n > -10 && n < 0)
-			_putchar('0' - n);
-		else
-		{
-			_putchar('0' + n);
-		}
-		if (n != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		printInt(n);
+		_putchar(',');
+		_putchar(' ');
 		if (n < 98)
 			n++;
 		else if (n > 98)
@@ -40,13 +22,58 @@ void print_to_98(int n)
 	}
 	if (n == 98)
 	{
-		_putchar('9');
-		_putchar('8');
+		printInt(98)
 		_putchar('\n');
 	}
 }
 
-void printThreeDigits(n)
+void printInt(int n)
 {
-	
+	bool isNegative;
+
+	if (n < 0)
+	{
+		isNegative = true;
+		_putchar('-');
+	}
+	else
+		isNegative = false;
+
+	if (isNegative)
+		n *= -1;
+	if (countDigits(n) == 1)
+		_putchar('0' + n)
+	else if (countDigits(n) == 2)
+	{
+		_putchar('0' + ((n - (n % 10)) / 10));
+		_putchar('0' + (n % 10));
+	}
+	else if (countDigits(n) == 3)
+	{
+		int ones = n % 10;
+		int tens = (n - ones) / 10;
+		int hundreds = (n - tens) / 100;
+
+		_putchar('0' + hundreds);
+		_putchar('0' + tens);
+		_putchar('0' + ones);
+	}
+	if (isNegative)
+		n *= -1;
+}
+
+int countDigits(int x) {
+	int y = x;
+	int count = 0;
+
+	if (y == 0)
+		return 1;
+	if (y < 0)
+		y = -y;
+	while (y!= 0)
+	{
+		y /= 10;
+		count++;
+	}
+	return count;
 }
