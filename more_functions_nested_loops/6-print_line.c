1f273c8ad9947d6833cdcce67ec9de18cc330799
root <dastelljes@outlook.com>
2024-05-22 23:47:09 +0000

    commit msg

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a linethat is @n characters long, and a new line.
+ * @n: number of underscores that are printed in order to draw the line.
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int r = 0;
+	while (r < n)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
