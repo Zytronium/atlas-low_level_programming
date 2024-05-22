3ba4f73e768e4887f6ae84e090c46a1f7b58ffff
root <dastelljes@outlook.com>
2024-05-22 18:33:39 +0000

    commit msg

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,9 +3,8 @@
  * mul - multiplies 2 numbers and returns the product
  * @a: number to be multiplied by @b
  * @b: number to be multiplied by @a
- * Returns: always @a multiplied by @b
+ * Return: always @a multiplied by @b
  */
-
 int mul(int a, int b)
 {
 	return (a * b);
