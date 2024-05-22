800ab0d6d1bd66f74b9c17e4524eeae6bbb5a35c
root <dastelljes@outlook.com>
2024-05-22 18:32:13 +0000

    commit msg

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+
+ * mul - multiplies 2 numbers and returns the product
+
+ * @a: number to be multiplied by @b
+
+ * @b: number to be multiplied by @a
+
+ * Returns: always @a multiplied by @b
+
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
