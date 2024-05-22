f33dd7a24df1615e42620cc824500291d14158e3
root <dastelljes@outlook.com>
2024-05-22 23:31:10 +0000

    commit msg

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,8 +14,8 @@ void more_numbers(void)
 		for (n = 0; n <= 14; n++)
 		{
 			if (n >= 10);
-				_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
+				_putchar('0' + (n / 10));
+			_putchar('0' + (n % 10));
 		}
 		_putchar('\n');
 		repeats++;
