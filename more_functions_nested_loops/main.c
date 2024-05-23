4de1198c5a8d062c367732f1cbecdeeb0c102f64
root <dastelljes@outlook.com>
2024-05-23 03:38:40 +0000

    commit msg

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -8,8 +8,12 @@
 int main(void)
 {
 	print_number(1712356);
+	putchar('\n');
 	print_number(01);
+	putchar('\n');
 	print_number(-95);
+	putchar('\n');
 	print_number(98);
+	putchar('\n');
 	return (0);
 }
