b7ec6e72052d24a90df520a213afeea3e7590b63
root <dastelljes@outlook.com>
2024-05-23 01:07:43 +0000

    commit msg

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,28 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * main - descr.
+ * main - prints 1 - 100 seperated by spaces, except:
+ * if the number is divisible by 3, print 'Fizz' instead,
+ * if the number is divisible by 5, print 'Buzz' instead,
+ * if it is divisible by both, print 'FizzBuzz' instead.
  *
  * Return: 0
  */
 int main(void)
 {
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (!(i % 3))
+			printf("Fizz");
+		if (!(i % 5))
+			printf("Buzz");
+		if (i % 5 && i % 3)
+			printf("%d", i);
+		if (i < 100)
+			putchar(' ');
+	}
+	putchar('\n');
+
 	return (0);
 }
