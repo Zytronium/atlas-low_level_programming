b9395243b427e81c7884e06aa59f9cd99fc2ac95
root <dastelljes@outlook.com>
2024-05-23 03:35:58 +0000

    commit msg

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -36,8 +36,11 @@ void print_number(int n)
  */
 int countDigits(int x)
 {
-	if (x < 0 && x > -10)
-		return (1);
+	if (x < 0)
+	{
+		if(x > -10)
+			return (1);
+	}
 	else if (x < 10)
 		return (1);
 
