933617535e70df648acd6a21a2d9a3dfe6f91517
root <dastelljes@outlook.com>
2024-05-24 22:19:53 +0000

    commit msg

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - changes the number given to 98 using a pointer.
- * @*n int to be changed to 98
+ * @n int to be changed to 98
  * Return: 0
  */
 void reset_to_98(int *n)
