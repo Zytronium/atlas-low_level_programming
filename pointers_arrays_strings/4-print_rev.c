f37fb8c1b44ffa15eab85fafdd4b8f3bc62f2177
root <dastelljes@outlook.com>
2024-05-24 22:59:35 +0000

    commit msg

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - descr.
- *
+ * print_rev - dprints a string/text in reverse, followed by a new line, to stdout.
+ * @s: text to be printed in reverse
  * Return: 0
  */
-int main(void)
+void print_rev(char *s)
 {
-	return (0);
+	
 }
