1390fd0bf1f697bc66714523e446a601d201c61c
root <dastelljes@outlook.com>
2024-05-24 23:38:06 +0000

    commit msg

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -29,3 +29,19 @@ void rev_string(char *s)
 	}
 	_puts("reversed: \"$reversed\"\ns: \"$s\"\n");
 }
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: string/text to be printed.
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
