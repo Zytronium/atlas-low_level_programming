64d4644296c5e4ea9ea04a3bbae0b640c84c32bd
root <dastelljes@outlook.com>
2024-05-24 22:24:05 +0000

    commit msg

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 /**
  * swap_int - swaps the values of 2 numbers, a and b.
  * @a: number given that will swap with @b
- * @b number given that will swap with @a
- * Return: 0
+ * @b: number given that will swap with @a
+ * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
