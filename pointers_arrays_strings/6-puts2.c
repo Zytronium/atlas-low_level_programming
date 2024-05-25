5469fa87757dfdcf2d14d3b04568ff221b6ae80c
Daniel Stelljes <dastelljes@outlook.com>
2024-05-25 17:50:09 -0500

    commit msg

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be used
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i < sizeOf(str))
+	{
+		if (!(i % 2))
+		_putchar(str[i]);
+		i++;
+	}
+	return (0);
+}
