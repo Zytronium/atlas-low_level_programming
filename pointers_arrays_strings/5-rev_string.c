b7a1ac190eb4a509765d7c33e2f4e5cfdffb024b
Daniel Stelljes <dastelljes@outlook.com>
2024-05-24 20:48:29 -0500

    commit msg

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,28 +8,26 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int n = 0;
-
+	char *reversed = s;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	char reversed[i] = s;
-	/*int size = i;*/
 
 	while (i > 0)
 	{
 		i--;
 		reversed[i] = s[n];
 		n++;
-		_putst(reversed, s);
+		_putst(reversed, s); //debug thing
 	}
 	while (n > 0)
 	{
 		n--;
 		s[n] = reversed[n];
-		_putst(reversed, s);
+		_putst(reversed, s); //debug thing
 	}
-	_putst(reversed, s);
+	_putst(reversed, s); //debug thing
 }
 /**
  * _puts - prints a string, followed by a new line, to stdout.
