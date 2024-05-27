44b27548bc0c756b3fedb9623d766762d88817e3
Daniel Stelljes <dastelljes@outlook.com>
2024-05-27 16:12:19 -0500

    commit msg

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,14 +10,14 @@ int _atoi(char *s)
 	int neg = 1;
 	int size;
 
-	for (size = 0; s[size] != '\0'; size++;)
+	for (size = 0; s[size] != '\0'; size++)
 	{
 		if (s[size] == '-')
 			neg *= -1;
 		else if (s[size] >= 48 && s[size] <= 57)
 		{
 			i *= 10;
-			i += s[size] - 48
+			i += s[size] - 48;
 		}
 
 	}
