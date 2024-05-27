edb9061ae957e4d332f302828e1058935330f703
Daniel Stelljes <dastelljes@outlook.com>
2024-05-27 16:27:11 -0500

    commit msg

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,32 @@
 #include "main.h"
 /**
- * _atoi - descr.
- * @s:
+ * _atoi - returns the integer version of a string I guess
+ * @s: string
  * Return: i
  */
 int _atoi(char *s)
 {
 	int i = 0;
+	int numberStarted = 0;
+	int numberEnded = 0;
 	int neg = 1;
 	int size;
 
 	for (size = 0; s[size] != '\0'; size++)
 	{
-		if (s[size] == '-')
-			neg *= -1;
-		else if (s[size] >= 48 && s[size] <= 57)
+		if (!numberEnded)
 		{
-			i *= 10;
-			i += s[size] - 48;
+			if (s[size] == '-' && !numberStarted)
+				neg *= -1;
+			else if (s[size] >= 48 && s[size] <= 57 && !numberEnded)
+			{
+				if (!numberStarted)
+					numberStarted = 1;
+				i *= 10;
+				i += s[size] - 48;
+			}
+			if (numberStarted && !numberEnded && (s[size] < 48 || s[size] > 57)
+					numberEnded = 1;
 		}
 
 	}
