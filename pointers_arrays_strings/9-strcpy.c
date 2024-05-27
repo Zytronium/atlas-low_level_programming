f156254b98bbda7f785965e6f6e5db11446d839c
Daniel Stelljes <dastelljes@outlook.com>
2024-05-27 15:38:45 -0500

    commit msg

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcpy - copies src and sets dest's value to that value
+ * @dest: destination string to be copied to
+ * @src: source string to be copied from
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src
+	return (*dest);
+}
