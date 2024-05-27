68e0b1f74efc9e06516d5a97f29d971c3d258a27
Daniel Stelljes <dastelljes@outlook.com>
2024-05-27 16:08:12 -0500

    commit msg

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+include "main.h"
+/**
+ * _atoi - descr.
+ * @s:
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int neg = 1;
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++;)
+	{
+		if (s[size] == '-')
+			neg *= -1;
+		else if (s[size] >= 48 && s[size] <= 57)
+		{
+			i *= 10;
+			i += s[size] - 48
+		}
+
+	}
+	i *= neg;
+
+	return (i);
+}
