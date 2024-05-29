46709379c4f6689eb4a77e89259d30049a786526
Daniel Stelljes <dastelljes@outlook.com>
2024-05-29 16:56:39 -0500

    did all of task 1 hopefully

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strnncat - adds src to dest
+ * @dest: string that is getting @src added to it
+ * @src: string that is added to @dest
+ * @n: max bytes of src that will be added to dest
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int x = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (x <= i + 1 && x <= n)
+	{
+		dest[i + x] = src[x];
+		x++;
+	}
+
+	return (dest);
+}
