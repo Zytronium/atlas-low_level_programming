967154edade4930e9927ee8c6251298097dc7623
Daniel Stelljes <dastelljes@outlook.com>
2024-05-29 16:26:04 -0500

    found out why i+=2 worked. fixed a bug where i always == 8 (src's length including '\0') by changing src to dest in line 12 while loop

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,13 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int n = 0;
 
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 		{
 			i++;
 		}
-		i += 1;
 		while (n <= i + 1)
 		{
-			dest[i + 1 + n] = src[n];
+			dest[i + n] = src[n];
 			n++;
 		}
 
