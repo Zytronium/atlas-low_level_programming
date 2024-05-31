f3a5d1d53e5579ae10cf1a2a49b3769d1df801f7
Daniel Stelljes <dastelljes@outlook.com>
2024-05-31 17:53:28 -0500

    documentation fix and maybe code fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _strncpy - adds src to dest
- * @dest: string that is getting @src added to it
- * @src: string that is added to @dest
- * @n: max bytes of src that will be added to dest
+ * _strncpy - copeis src to dest
+ * @dest: string that is getting @src copied to it
+ * @src: string that is copied to @dest
+ * @n: max bytes of src that will be copied to dest
  * Return: dest
  */
- char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (dest[i] != '\0' && i < n)
+	while (src[i] != '\0' && i < n)
 	{
         dest[i] = src[i];
 		i++;
