06f399f8bbac75762700210313f9b46b80730862
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 01:50:47 -0500

    fixed #3 hopefully. I understand 3 now

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,22 @@
 #include "main.h"
 /**
- * _strcmp - returns the difference in the value of 2 strings' total values
+ * _strcmp - compares 2 strings and returns the difference
  * which is evaluated by adding up the ASCII codes of all chars in the strings
- * @s1: string 1
- * @s2: string 2
+ * @s1: string one
+ * @s2: string two
  *
- * Return: difference between strings
+ * Return: difference in the values of the 2 strings - <0, 0, or >0
  */
 int _strcmp(char *s1, char *s2)
 {
-    int s1Total;
-    int s2Total;
-    int i;
+	int i;
 
-    for (i = 0; s1[i] != '\0'; i++)
-    {
-        s1Total += s1[i];
-    }
-
-    for (i = 0; s2[i] != '\0'; i++)
-    {
-        s2Total += s2[i];
-    }
-
-	return (s2Total - s1Total);
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+		if (s1[i] - s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 }
