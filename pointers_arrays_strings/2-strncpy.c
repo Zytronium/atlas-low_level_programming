b92ed7de7f64d06c7a1b0f07d84d01e48114590d
Daniel Stelljes <dastelljes@outlook.com>
2024-05-31 19:04:57 -0500

    test2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,19 +8,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	// int i = 0;
-
-    // if (!n || src[0] == '\0')
-    // {
-    //     dest[0] = '\0';
-    // }
-
-	// while (dest[i] != '\0' && i < n)
-	// {
-    //     dest[i] = src[i];
-	// 	i++;
-	// }
-
     dest = src;
 
 	return (dest);
