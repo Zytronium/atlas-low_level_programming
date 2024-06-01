730e236d97d7b1b3402615397e6986d269920eac
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 17:42:59 -0500

    `while` loops are just inefficient repeating if statements if you want them to be.

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,10 +1,40 @@
 #include "main.h"
 /**
- * main - descr.
- *
- * Return: 0
+ * leet - turns the letters a e o t and l into 4 3 0 7 and 1, respectively.
+ * Not case sensitive.
+ * @str string to be modified
+ * Return: str
  */
-int main(void)
+char *leet(char *str)
 {
-	return (0);
+	int i = 0;
+	int j;
+	int aeotl = 0;
+	char letters[10] = "aeotlAEOTL";
+
+	while (str[i]) {
+		char c = str[i];
+			for (j = 0; letters[j]; j++)
+			{
+				if (c == letters[j])
+					aeotl = 1;
+			}
+		while (aeotl)
+		{
+			while (c == 'a' || c == 'A')
+				c = '4';
+			while (c == 'e' || c == 'E')
+				c = '3';
+			while (c == 'o' || c == 'O')
+				c = '0';
+			while (c == 't' || c == 'T')
+				c = '7';
+			while (c == 'l' || c == 'L')
+				c = '1';
+
+			aeotl = 0;
+			str[i] = c;
+		}
+	}
+	return (str);
 }
