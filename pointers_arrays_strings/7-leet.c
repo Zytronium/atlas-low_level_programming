12ddf14ca4144ae1a060c589b7f335c88c71dc65
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 18:07:53 -0500

    2 for loops, 1 if, and 2 strings to find and replace aeotl letters in the provided string with 43071, respectively.

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,41 +1,21 @@
 #include "main.h"
 /**
- * leet - turns the letters a e o t and l into 4 3 0 7 and 1, respectively.
+ * leet - turns the letrs a e o t and l into 4 3 0 7 and 1, respectively.
  * Not case sensitive.
- * @str string to be modified
- * Return: str
+ * I'm really proud of this script
+ * @s string to be modified
+ * Return: s converted to leet
  */
-char *leet(char *str)
+char *leet(char *s)
 {
-	int i = 0;
-	int j;
-	int aeotl = 0;
-	char letters[10] = "aeotlAEOTL";
+	int i, j;
+	char letrs[10] = "aeotl";
+	char numbs[10] = "43071";
 
-	while (str[i]) {
-		char c = str[i];
-			for (j = 0; letters[j]; j++)
-			{
-				if (c == letters[j])
-					aeotl = 1;
-			}
-		while (aeotl)
-		{
-			while (c == 'a' || c == 'A')
-				c = '4';
-			while (c == 'e' || c == 'E')
-				c = '3';
-			while (c == 'o' || c == 'O')
-				c = '0';
-			while (c == 't' || c == 'T')
-				c = '7';
-			while (c == 'l' || c == 'L')
-				c = '1';
-
-			aeotl = 0;
-			str[i] = c;
-		}
-		i++;
+	for (i = 0; s[i]; i++) {
+			for (j = 0; letrs[j]; j++)
+				if (s[i] == letrs[j] || s[i] == letrs[j] - 32)
+					s[i] = numbs[j];
 	}
-	return (str);
+	return (s);
 }
