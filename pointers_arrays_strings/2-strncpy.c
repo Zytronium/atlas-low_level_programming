087d9c090938fbadb30e8401fb09cd8234b125b9
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 12:59:31 -0500

    hopefully fixed #2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncpy - copeis src to dest
+ * _strncpy - copies src to dest
  * @dest: string that is getting @src copied to it
  * @src: string that is copied to @dest
  * @n: max bytes of src that will be copied to dest
@@ -10,12 +10,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-    if (!n || src[0] == '\0')
-    {
-        dest[0] = '\0';
-    }
 
-	while (src[i + 1] != '\0' && i < n)
+	if (i <= n)
+	{
+		dest[i] = '\0';
+	}
+
+	while (src[i] && i < n)
 	{
         dest[i] = src[i];
 		i++;
