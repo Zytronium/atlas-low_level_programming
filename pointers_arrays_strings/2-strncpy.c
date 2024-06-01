fa4002c77dd6aaf056c39124fb500def49ac22bb
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 13:30:03 -0500

    theory tested; type of loop makes no difference here

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,15 +8,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i =0;
+	int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
 		if (src[i])
         	dest[i] = src[i];
 		else
 			dest[i] = '\0';
-		i++;
 	}
 
 	return (dest);
