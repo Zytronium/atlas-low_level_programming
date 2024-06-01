e8d18bf84e68203b013f1fd15dbac51eeb957d29
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 14:27:20 -0500

    finished 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,27 @@
 #include "main.h"
 /**
  * cap_string - capitalizes the first letter of all words.
- * @s: what
- * Return: 0
+ * @s: string provided
+ * Return: @s but with every word's 1st letter capitalized
  */
 char *cap_string(char *s)
 {
-	int a = 0;
+	int cap = 0;
+	char separators[13] = " \t\n,;.!?\"({})";
 	int i;
+	int j;
 
 	for (i = 0; s[i]; i++)
-		if()
+	{
+		if (cap && s[i] >= 'a' && s[i] < 'z')
+			s[i] -= 32;
+		cap = 0;
+		for (j = 0; separators[j]; j++)
+		{
+			if (s[i] == separators[j])
+				cap = 1;
+		}
+	}
 
 	return (s);
 }
