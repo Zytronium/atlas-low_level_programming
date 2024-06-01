a090def8542b35c37fe1b2a10c7a57ab33d6ebd8
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 14:11:42 -0500

    started 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes the first letter of all words.
+ * @s: what
+ * Return: 0
+ */
+char *cap_string(char *s)
+{
+	int a = 0;
+	int i;
+
+	for (i = 0; s[i]; i++)
+		if()
+
+	return (s);
+}
