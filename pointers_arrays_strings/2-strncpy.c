e093201a29c0e1ce4794fe81cb77ed5f505ca558
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 13:00:54 -0500

    moved if condition

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	if (i <= n)
-	{
-		dest[i] = '\0';
-	}
-
 	while (src[i] && i < n)
 	{
         dest[i] = src[i];
 		i++;
+	}
+
+	if (i <= n)
+	{
+		dest[i] = '\0';
 	}
 
 	return (dest);
