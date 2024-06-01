fb219f9420a24501eaba934a9cdadb56d76432bf
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 13:42:01 -0500

    src[i] was out of bounds after src[i] == '\0', so the else statement had to be its own loop after the for loop it was in

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; src[i] && i < n; i++)
 	{
-		if (src[i])
-        	dest[i] = src[i];
-		else
-			dest[i] = '\0';
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
