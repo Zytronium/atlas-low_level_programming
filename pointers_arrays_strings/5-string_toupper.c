78f15d9b1ecc5b035e6fc903c2afea23fecc13b3
Daniel Stelljes <dastelljes@outlook.com>
2024-06-01 01:07:15 -0500

    started #5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * string_toupper - descr.
+ * @c: string I guess. It actually didn't give a name to use for this...
+ * Return: 0
+ */
+char *string_toupper(char *c)
+{
+
+	return (0);
+}
