0d732df0fbe9555dd2ea964f8a3d85d878980b0a
Daniel Stelljes <dastelljes@outlook.com>
2024-06-02 18:31:07 -0500

    did task 0-memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,11 +3,18 @@
  * _memset - fills memory with a constant byte, similar to
  * the standard library function memset.
  * @s: the memory location to be filled.
- * @b: a character, which is constant byte to fill the memory with
+ * @b: a character, which is constant byte to fill the memory with.
  * @n: max amount of bytes that will be filled.
- * Return: todoWip
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	int i = 0;
+
+	while (i <= n)
+	{
+		s[i] = b;
+		i++;
+	}
 	return (s);
 }
