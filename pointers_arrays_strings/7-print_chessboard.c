16e768d803bd359e2999b8361c618be5e3f1db16
Daniel Stelljes <dastelljes@outlook.com>
2024-06-03 18:07:32 -0500

    Because you are what you eat, 6 can't count, and 6 8 7 so that 7 couldn't scare 6 anymore.

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,17 @@
 #include "main.h"
 /**
- * print_cheeseboard - prints a chessboard.
+ * print_chesseboard - prints a chessboard.
  * @a: matrix / 2D array of chess board in characters
  * Return: 0
  */
 void print_chessboard(char (*a)[8])
 {
+	int x;
+	int y;
+
+	for (y = 0; y <= 8; y++)
+		for (x = 0; x <= 8; x++)
+			_putchar(a[x][y]);
+
 	return (0);
 }
