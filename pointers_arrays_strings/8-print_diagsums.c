9d2379f375ac6ea4253222ea65ad7a7efa21aaad
Daniel Stelljes <dastelljes@outlook.com>
2024-06-03 23:57:52 -0500

    last second work

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,10 +2,22 @@
 #include <stdio.h>
 /**
  * print_diagsums - descr.
- * @a:
- * @size:
+ * @a: matrix/array provided
+ * @size: size of the array @a
  * Return: 0
  */
 void print_diagsums(int *a, int size)
 {
+	int m;
+	int n;
+	int i = 0;
+
+	while (i < size)
+	{
+		m += a[i * size + i];
+		n += a[i * size + (size - (i + 1))];
+		i++;
+	}
+
+	printf("%d, %d\n", m, n);
 }
