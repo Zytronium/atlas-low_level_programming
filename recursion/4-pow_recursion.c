b2796a8f7d90e9c90e37fa8e8250f861bd0bf0ae
Daniel Stelljes <dastelljes@outlook.com>
2024-06-05 18:25:40 -0500

    betty didn't like it. Fixed it

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - does exponents.
+ * _pow_recursion - descr.
  *
  * @x: number to be raised to the power of @y
  * @y: number to be used to raise @x to the power of this
@@ -12,5 +12,9 @@ int _pow_recursion(int x, int y)
 {
 	if (y > 1)
 		return (x * _pow_recursion(x, y - 1));
-	else return (y < 0) ? -1 : 1;
+	else
+		if (y > 0)
+		return (1);
+	else
+		return (-1);
 }
