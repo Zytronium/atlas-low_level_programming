65428ad83dacdd3f69cb185867b15feb01ec87e4
Daniel Stelljes <dastelljes@outlook.com>
2024-06-05 18:00:31 -0500

    did task 3

--- recursion/main.c
+++ recursion/main.c
@@ -8,6 +8,15 @@
  */
 int main(void)
 {
-	printf("%d\n", _strlen_recursion("Denial Stebebblejebes"));
+	int r;
+
+	r = factorial(1);
+	printf("%d\n", r);
+	r = factorial(5);
+	printf("%d\n", r);
+	r = factorial(10);
+	printf("%d\n", r);
+	r = factorial(-1024);
+	printf("%d\n", r);
 	return (0);
 }
