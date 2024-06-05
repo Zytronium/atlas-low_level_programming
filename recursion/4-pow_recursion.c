cd953c7afa6ceb733e6d7b0fe7d64c55e2193f51
Daniel Stelljes <dastelljes@outlook.com>
2024-06-05 18:30:41 -0500

    changed > to >= in both if statements

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - descr.
+ * _pow_recursion - does exponents on a number by returning @x^@y.
  *
  * @x: number to be raised to the power of @y
  * @y: number to be used to raise @x to the power of this
@@ -10,10 +10,10 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y > 1)
+	if (y >= 1)
 		return (x * _pow_recursion(x, y - 1));
 	else
-		if (y > 0)
+		if (y >= 0)
 		return (1);
 	else
 		return (-1);
