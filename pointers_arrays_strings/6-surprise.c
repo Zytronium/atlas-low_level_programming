f4c1fe7ffb307d60d11d000fb4e7e77d57b1c61f
Daniel Stelljes <dastelljes@outlook.com>
2024-06-04 19:47:40 -0500

    typo correction

--- pointers_arrays_strings/6-surprise.c
+++ pointers_arrays_strings/6-surprise.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * repeat_print_surprise - prints a surprise a certain number of times.
- * file 6 was skipped, so I made this hoke to fill in
+ * file 6 was skipped, so I made this joke to fill in
  * the gap between task 5 and 6 (files 5 and 7).
  *
  * @repeat: number of time to repeat printing the surprise
