fe6cebcc81802e09f8cdbfd11874ddf13b0d340b
Daniel Stelljes <dastelljes@outlook.com>
2024-06-05 15:58:27 -0500

    took a lot of trial and error (sorry-forgot to commit previous attempts), but I think I got task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,11 +6,20 @@
  * give a log of some small additions I made so
  * you can can see my thought process and stuff
  * up on GitHub. Now, read the 1st word of every line.
+ * Update: I read what to do and it's just printing a string using recursion.
+ * I just couldn't delete my masterpiece above.
  *
- * @s: s
- * Return: 0
+ * @s: string to print.
+ *
+ * Return: nothing.
  */
 void _puts_recursion(char *s)
 {
-	return (0);
+	if (!*s)
+		return;
+	char *ptr = s;
+	int i = ptr++;
+
+	_putchar(*s);
+	_puts_recursion(ptr);
 }
