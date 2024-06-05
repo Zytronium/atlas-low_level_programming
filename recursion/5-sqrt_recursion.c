6803f4d5607ee13678dc060ab726d628cfcaac58
Daniel Stelljes <dastelljes@outlook.com>
2024-06-05 18:43:44 -0500

    skipping task 5 for now

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - gets the natural square root of a number @n.
+ *
+ * @n: input number to get the natural square root of
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	/*skipping, just 4 now*/
+	return (0);
+}
