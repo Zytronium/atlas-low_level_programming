6ed2187a2390a8319a7f847e45a1eb4803be8d96
Daniel Stelljes <dastelljes@outlook.com>
2024-06-05 17:47:54 -0500

    created and completed task2

--- recursion/main.c
+++ recursion/main.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - Check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	_print_rev_recursion("\nDaniel Stebebblejebes");
+	printf("%d\n", _strlen_recursion("Denial Stebebblejebes"));
 	return (0);
 }
