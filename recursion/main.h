e8feb61639a1903fb880ec8dbc1fdd9a1d6821a0
Daniel Stelljes <dastelljes@outlook.com>
2024-06-06 14:04:08 -0500

    made second function return long long

--- recursion/main.h
+++ recursion/main.h
@@ -7,7 +7,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-long int _sqrt_part_two(int n, int m);
+long long _sqrt_part_two(int n, int m);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
