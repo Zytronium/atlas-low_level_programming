6afe0480a6a55a057ab9cbb1ad96be9eaf715e37
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-10 02:15:00 -0500

    updated main.h

--- recursion/main.h
+++ recursion/main.h
@@ -14,5 +14,6 @@ int prime_helper(int n, int guess);
 int is_palindrome(char *s);
 int palindrome_helper(char *string, int index1, int index2);
 int wildcmp(char *s1, char *s2);
+int wildcmp_helper(char *s1, char *s2, int index);
 
 #endif
