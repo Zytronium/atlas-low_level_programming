2ce325702704b6d0ba391b6144c275f2ac8792d0
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-09 19:14:33 -0500

    did task 0

--- TestRunScript.bash
+++ TestRunScript.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd ./recursion/
-gcc main.c _putchar.c *-*.c -o testRun -Wall -Werror -Wextra -pedantic -std=gnu89
+cd ./static_libraries/
+gcc _putchar.c *-*.c -o testRun -Wall -Werror -Wextra -pedantic -std=gnu89
 ./testRun
 rm testRun
---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: the number of arguments supplied to argv[]
+ * @argv: arguments supplied to main, including the name of the program
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
