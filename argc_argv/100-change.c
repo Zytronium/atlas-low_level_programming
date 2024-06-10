d20d8871bff31ddb0784e35f63cb155c9092a237
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-09 22:03:38 -0500

    Did advanced task 5

---
+++ argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the minimum number of coins to make change for an amount of money
+ *
+ * @argc: the number of arguments supplied to argv[]
+ * @argv: arguments supplied to main, including the name of the program
+ *
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int cents = atoi(argv[1]);
+	int coins = 0;
+
+	if (cents < 0)
+		cents = 0;
+
+	while (cents > 0)
+	{
+		if (cents - 25 >= 0)
+			cents -= 25;
+		else if (cents - 10 >= 0)
+			cents -= 10;
+		else if (cents - 5 >= 0)
+			cents -= 5;
+		else if (cents - 2 >= 0)
+			cents -= 2;
+		else
+			cents--;
+		coins++;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
