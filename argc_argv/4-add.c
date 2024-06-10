df303cbe2129b5abd4b73e84c1219f50b3fce773
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-09 20:23:12 -0500

    allowed for arbitrary number of positive numbers to add.

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,30 +8,35 @@
  * @argc: number of arguments supplied to argv[], including the program name
  * @argv: arguments supplied to main, including the program's name
  *
- * Return: 0 on success
+ * Return: 0 on success, 1 on error.
 */
 int main(int argc, char *argv[])
 {
-	int x = atoi(argv[1]);
-	int y = atoi(argv[2]);
+	int i;
+	unsigned int sum = 0;
 
-	/*
-	 * If there are not exactly 2 supplied arguments (3 including program name)
-	 * or if either of the other 2 supplied arguments are not zero, but
-	 * atoi(that argument) is zero (which means atoi gave an error, which means
-	 * there was a non-number character (besides + or - signs) in the string),
-	 * or if one of the 2 arguments starts with '-', then:
-	 * Print "Error" with a newline, and return 1.
-	 */
-	if (
-			argc != 3 || (x == 0 && argv[1][0] != '0')
-			|| (y == 0 && argv[2][0] != '0')
-			|| argv[1][0] == '-'
-			|| argv[2][0] == '-'
-			)
+	if (argc < 3)
 	{
-		printf("Error\n");
-		return (1);
+	printf("Error\n");
+	return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		/*
+		 * If any of the supplied numbers to add are not zero, but
+		 * atoi(that argument) is zero (which means atoi gave an error,
+		 * which means there was a non-number character (besides + or - signs)
+		 * in the string), or if any of the numbers starts with '-', then:
+		 * Print "Error" with a newline, and return 1.
+		 */
+		if ((atoi(argv[i]) == 0 && argv[i][0] != '0') || argv[i][0] == '-')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", (atoi(argv[1]) + atoi(argv[2])));
