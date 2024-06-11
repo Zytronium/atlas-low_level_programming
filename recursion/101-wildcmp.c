f34ffcdb5c8d1c7e470b0b9e0dbfc1329270ceac
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-10 22:41:20 -0500

    why does my debug code result in the program saying that there is a "." character inside the "**" string

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
-int while_loop_helper(char *str, int i, char c)
+int while_loop_helper(char *str, int i, char c, int equals)
 {
-	if (str[i] != c)
+	if (equals)
 	{
-		i = (while_loop_helper(str, i + 1, c));
+		if (str[i] != c)
+		{
+			i = (while_loop_helper(str, i + 1, c, equals));
+		}
 	}
+	else
+		if (str[i] == c)
+		{
+			i = (while_loop_helper(str, i + 1, c, equals));
+		}
+
 	return (i);
 }
 
@@ -22,6 +31,7 @@ int while_loop_helper(char *str, int i, char c)
 */
 int wildcmp_helper(char *s1, char *s2, int i1, int i2)
 {
+	int j;
 	/*if (s1[i1] == '\0')
 	{
 		_putchar('\\');
@@ -45,23 +55,49 @@ int wildcmp_helper(char *s1, char *s2, int i1, int i2)
 	if ((!s1[i1] || !s2[i2]) && s2[i2] != '*')
 	{
 		_putchar('\n');
-		if (s1[i1] == s2[i2])
-			return (1);
-		return (0);
+		/*if (s1[i1] == s2[i2])*/
+			return (11);
+		/*return (30);*/
+	}
+
+	if (s1[i1] != s2[i2] && s2[i2] != '*') {
+		_putchar('\n');
+		_putchar(s2[i2]);
+		return (40);
+	}
+	_putchar('\n');
+	_putchar('\n');
+
+
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		_putchar(s2[j]);
 	}
 
 	if (s2[i2] == '*')
 	{
-		i2 = while_loop_helper(s2, i2,'*');
-		i1 = while_loop_helper(s1, i1, s2[i2 + 1]);
+		if (!s2[i2 + 1])
+		{
+			_putchar('\n');
+			return (2);
+		}
+		i2 = while_loop_helper(s2, i2,'*', 0);
+		i1 = while_loop_helper(s1, i1, s2[i2 + 1], 1);
+		if (!s2[i2 + 1])
+		{
+			_putchar('\n');
+			return (3);
+		}
 
-		if (!s1[i1] && !s2[i2 + 1]){
+		if (!s1[i1] && !s2[i2 + 1])
+		{
 			_putchar('\n');
 			return (1);
 		}
-		if (!s1[i1]){
+		if (!s1[i1])
+		{
 			_putchar('\n');
-			return (0);
+			return (20);
 		}
 		return wildcmp_helper(s1, s2, i1, i2 + 1);
 
@@ -78,10 +114,7 @@ int wildcmp_helper(char *s1, char *s2, int i1, int i2)
 		return wildcmp_helper(s1, s2, i1 + 1, i2);
 	}*/
 
-	if (s1[i1] != s2[i2] && s2[i2] != '*') {
-		_putchar('\n');
-		return (0);
-	}
+
 
 	return wildcmp_helper(s1, s2, i1 + 1, i2 + 1);
 	/*DONE(?): allow any string of ANY LENGTH to be in place of this single '*' char*/
