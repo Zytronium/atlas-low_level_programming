95d53a4d6d475d766c5ebce7d9428153f01f105c
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-10 20:48:15 -0500

    my brain hurts

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,61 +1,62 @@
 #include "main.h"
 
-int wildcmp_helper(char *s1, char *s2, int index1, int index2)
+int wildcmp_helper(char *s1, char *s2, int i1, int i2)
 {
-	if (s1[index1] == '\0')
+	/*if (s1[i1] == '\0')
 	{
 		_putchar('\\');
 		_putchar('0');
 	}
 	else
-		_putchar(s1[index1]);
+		_putchar(s1[i1]);
 
 	_putchar('\t');
 
-	if (s2[index2] == '\0')
+	if (s2[i2] == '\0')
 	{
 		_putchar('\\');
 		_putchar('0');
 	}
 	else
-		_putchar(s2[index2]);
+		_putchar(s2[i2]);
 
-	_putchar('\n');
+	_putchar('\n');*/
 
 
-	if (s1[index1] == '\0') {
+	if ((!s1[i1] || !s2[i2]) && s2[i2] != '*')
+	{
 		_putchar('\n');
-		if (s1[index1] != s2[index2])
-			return (0);
-		return (1);
+		if (s1[i1] == s2[i2])
+			return (1);
+		return (0);
 	}
 
-	if (s2[index2] == '*')
+	if (s2[i2] == '*')
 	{
-		return wildcmp_helper(s1, s2, index1, index2 + 1);
-		/*if (s2[index2 + 1] == '*')
-			return wildcmp_helper(s1, s2, index1, index2 + 1);
-		if (s1[index1 + 1] == s2[index2 + 1])
-			return wildcmp_helper(s1, s2, index1 + 1, index2 + 1);
-		return wildcmp_helper(s1, s2, index1 + 1, index2);*/
+		return wildcmp_helper(s1, s2, i1, i2 + 1);
+		/*if (s2[i2 + 1] == '*')
+			return wildcmp_helper(s1, s2, i1, i2 + 1);
+		if (s1[i1 + 1] == s2[i2 + 1])
+			return wildcmp_helper(s1, s2, i1 + 1, i2 + 1);
+		return wildcmp_helper(s1, s2, i1 + 1, i2);*/
 	}
-	if (s2[index2 - 1] == '*')
+	if (s2[i2 - 1] == '*')
 	{
-		if (s1[index1] == s2[index2])
-			return wildcmp_helper(s1, s2, index1 + 1, index2 + 1);
-		return wildcmp_helper(s1, s2, index1 + 1, index2);
+		if (s1[i1] == s2[i2])
+			return wildcmp_helper(s1, s2, i1 + 1, i2 + 1);
+		return wildcmp_helper(s1, s2, i1 + 1, i2);
 	}
 
-	/*if (s1[index1] != s2[index2] && s2[index2] != '*') {
+	/*if (s1[i1] != s2[i2] && s2[i2] != '*') {
 		_putchar('\n');
 		return (0);
 	}*/
 
-	return wildcmp_helper(s1, s2, index1 + 1, index2 + 1);
+	return wildcmp_helper(s1, s2, i1 + 1, i2 + 1);
 	/*DONE(?): allow any string of ANY LENGTH to be in place of this single '*' char*/
 	/*
 	* [✔] create 2 index variables instead of 1
-	* [✔] index1++ until s1[index1] == s2[index2]
+	* [✔] i1++ until s1[i1] == s2[i2]
 	* [✖] may or may not need a second helper function to avoid loops, but most likely NOT
 	*/
 }
@@ -70,6 +71,6 @@ int wildcmp_helper(char *s1, char *s2, int index1, int index2)
 */
 int wildcmp(char *s1, char *s2)
 {
-	_putchar('\n');
+	/*_putchar('\n');*/
 	return (wildcmp_helper(s1, s2, 0, 0));
 }
