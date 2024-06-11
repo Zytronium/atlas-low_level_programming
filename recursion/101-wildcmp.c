883c3327e37833b99d277a91c39f56d7ebbd92d8
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-10 23:33:27 -0500

    fixed my broken code more

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -88,7 +88,7 @@ int wildcmp_helper(char *s1, char *s2, int i1, int i2)
 			return (1);
 		}
 
-		i2 = while_loop_helper(s2, i2,'*', 0);
+		i2 = while_loop_helper(s2, i2,'*', 0) - 1;
 		i1 = while_loop_helper(s1, i1, s2[i2 + 1], 1);
 
 		/*printf("i1: %d\n", i1);
