1350b0c5460e918c3faa41c97b66fbf93b55fee2
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-10 23:55:11 -0500

    betty (?)

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -56,7 +56,6 @@ int wildcmp_helper(char *s1, char *s2, int i1, int i2)
 			return (1);
 		return (0);
 	}
-
 	if (s1[i1] != s2[i2] && s2[i2] != '*')
 	{
 		if (s2[i2 + 1] == '*')
@@ -64,7 +63,6 @@ int wildcmp_helper(char *s1, char *s2, int i1, int i2)
 		else
 			return (1);
 	}
-
 	if (s2[i2] == '*')
 	{
 		if (!s2[i2 + 1])
@@ -81,7 +79,7 @@ int wildcmp_helper(char *s1, char *s2, int i1, int i2)
  *
  * @s1: string to compare to s2
  * @s2: string to compare to s1
- * Return: 1 if both strings are equal, 2 if not.
+ * Return: 1 if both strings are equal, 2 if not
 */
 int wildcmp(char *s1, char *s2)
 {
