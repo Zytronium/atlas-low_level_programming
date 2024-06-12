6fc009e0e7659576682ac4f24b6018d6cbca4680
Zytron1228 <dastelljes@outlook.com>
2024-06-12 17:59:46 -0500

    did task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * _strdup - descr.
  *
@@ -8,5 +10,22 @@
  */
 char *_strdup(char *str)
 {
-	return (0);
+	char *newStr;
+	int strLen = 0;
+	int i;
+
+	while (str[strLen])
+		strLen++
+
+	newStr = malloc(sizeof(char) * strLen);
+
+	if (!newStr)
+		return (NULL);
+
+	for (i = 0; i <= strLen; i++)
+	{
+		newStr[i] = str[i];
+	}
+
+	return (newSt);
 }
