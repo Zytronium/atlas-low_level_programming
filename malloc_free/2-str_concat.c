bafc6d1cafce08acf6147b0c3513e4dee7c08159
Zytron1228 <dastelljes@outlook.com>
2024-06-12 18:30:04 -0500

    Did task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * str_concat - concatenates 2 strings and returns the result
+ *
+ * @s1: string to have another string added to it
+ * @s2: string to be added to the other string
+ *
+ * Return: new string that's equal to s1 plus s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *newStr;
+	int str1Len = 0, str2Len = 0, newStrLen, i;
+
+	if (!s1 || !s2)
+		return (NULL);
+
+	while (s1[str1Len])
+	{
+		str1Len++
+	}
+
+	while (s2[str2Len])
+	{
+		str2Len++;
+	}
+	str2Len++;
+
+	newStrLen = str2Len + str1Len;
+
+	newStr = malloc(sizeof(char) * newStrLen);
+
+	if (!newStr)
+		return (NULL);
+
+	for (i = 0; i <= newStrLen; i++)
+	{
+		if (i <= str1Len)
+			newStr[i] = s1[i];
+		else newStr = s2[i - str1Len];
+	}
+
+	return (newStr);
+}
