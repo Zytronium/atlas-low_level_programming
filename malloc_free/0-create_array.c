367d685529b17509440e95bc6fb970d59072525b
Zytron1228 <dastelljes@outlook.com>
2024-06-12 17:19:47 -0500

    fix????

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,9 +19,8 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 		str[i] = c;
-	str[size] = '\0';
 
 	return (str);
 }
