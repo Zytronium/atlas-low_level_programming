bbd90c18b82cb74bb5eb7afbae4135929911faa5
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-14 18:56:38 -0500

    betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,42 +15,28 @@ char *str_concat(char *s1, char *s2)
 	int str1Len = 0, str2Len = 0, newStrLen, i;
 
 	if (s1 != NULL)
-	{
 		while (s1[str1Len])
-		{
 			str1Len++;
-		}
-	}
 	else
 		s1 = "";
 	if (s2 != NULL)
-	{
 		while (s2[str2Len])
-		{
 			str2Len++;
-		}
-	}
 	else
 		s2 = "";
-
 	newStrLen = str1Len + str2Len;
-
 	newStr = malloc(sizeof(char) * newStrLen + 1);
 	if (newStr == NULL)
 		return (NULL);
-
 	for (i = 0; i <= newStrLen + 1; i++)
 	{
 		if (i < str1Len && str1Len > 0)
 			newStr[i] = s1[i];
 		else
 			if (i >= str1Len)
-			{
 				newStr[i] = s2[i - str1Len];
-			}
 		else
 			newStr[i] = '\0';
 	}
-
 	return (newStr);
 }
