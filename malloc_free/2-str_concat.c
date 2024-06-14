ff4d4a9f8cc9e734485735f3c1c43474294257ea
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-14 16:22:46 -0500

    another memory allocation problem theory test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -44,7 +44,7 @@ char *str_concat(char *s1, char *s2)
 		{
 			str2Len++;
 		}
-		str2 = malloc(sizeof(char) * str2Len);
+		str2 = malloc(sizeof(char) * str2Len + 1);
 		for (i = 0; s2[i]; i++)
 		{
 			str2[i] = s2[i];
