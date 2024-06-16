9c3912699189606655ccf5aa3a8cec88ff0239f9
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-16 17:53:56 -0500

    betty

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
-#ifndef ABS(x)
-#define ABS(x) (((x) >= 0) ? (x) : -(x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
+#define ABS(x) (((x) >= 0) ? (x) : -(x))
 int main(void);
 
 #endif
