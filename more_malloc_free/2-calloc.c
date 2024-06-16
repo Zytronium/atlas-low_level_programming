89c474f30ddbee0b129a59c5a1ed5dbf181a0dc5
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-15 21:23:39 -0500

    did task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * with @nmemb elements of a type that's @size bytes
+ *
+ * @nmemb: number of members of the array.
+ * @size: number of bytes for each member of array.
+ * basically sizeof(type of members in array)
+ *
+ * Return: pointer to first member of the newly allocated array
+ * or null if fails or nmemb or size is 0
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array;
+	unsigned int i = 0;
+	char *ptrArr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	array = malloc(size * nmemb);
+	if (array == NULL)
+		return (NULL);
+
+	ptrArr = array;
+
+	while (i < nmemb)
+	{
+		ptrArr[i] = 0;
+		i++;
+	}
+
+	return (array);
+}
