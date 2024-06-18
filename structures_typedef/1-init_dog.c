1d6b015b8daf02efc863494a66dd7814a9bd1679
Zytron1228 <dastelljes@outlook.com>
2024-06-18 17:56:30 -0500

    idk what they really want me to do

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initiates the dog
@@ -12,6 +13,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(name) + sizeof(float) + sizeof(owner));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
