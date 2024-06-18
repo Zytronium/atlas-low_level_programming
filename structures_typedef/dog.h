f11cc1130edbb6823a82e48f614cd2d5e1e9432d
Zytron1228 <dastelljes@outlook.com>
2024-06-18 17:31:32 -0500

    README.md

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog - info for a dog
+ *
+ * @name: dog's name (string)
+ * @age: dog's age (number)
+ * @owner: dog's owner's name (string)
+ */
 typedef struct dog
 {
-	char * name;
+	char *name;
 	float age;
 	char *owner;
 } dog;
