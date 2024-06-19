20f87e89daa423db24d757e2911c25414593524b
Zytron1228 <dastelljes@outlook.com>
2024-06-19 18:51:54 -0500

    playing with dog is one thing; creating new dog is playing dog spelled backwards

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: new dog's owner's name
+ *
+ * Return: 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t my_dog;
+	char *new_name, *new_owner;
+
+	new_name = malloc(strlen(name) + 1);
+	new_owner = malloc(strlen(owner) + 1);
+
+	if (new_name == NULL || new_owner == NULL)
+		return (NULL);
+
+	init_dog(&my_dog, strcpy(new_name, name), age, strcpy(new_owner, owner));
+
+	return (&my_dog);
+}
--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /**
- * free_dog - it's like Free Bird but with a dog instead of a bird. Duh
+ * free_dog - frees a dog (from memory)
+ * it's like Free Bird but with a dog instead of a bird. Duh
  *
  * @d: dog to be set free
  *
- * Return: Nothing. In fact, less than nothing. It DELETES A DOG!
+ * Return: Nothing. In fact, less than nothing. IT DELETES A DOG!
  * #FREE_DOG_DOES_NOT_FREE_DOGS_THE_WAY_YOU_THINK_IT_DOES
  */
 void free_dog(dog_t *d)
