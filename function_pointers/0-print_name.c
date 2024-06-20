bd3f45824c03932392cab3e1dabb96c876e216b1
Zytron1228 <dastelljes@outlook.com>
2024-06-20 18:08:44 -0500

    started 1

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - performs an action on a name string, then doesn't print it.
  * (because for some reason I get it wrong if it prints the name)
  *
- * @name: string to modify and print
+ * @name: string to modify
  * @f: pointer to function that modifies name
  *
  * Return: Nothing
---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * main - descr
+ *
+ * @param1:
+ *
+ * Return: 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	return (0);
+}
