cf74388fae01a2a1b20eec4270ebccb67a9f0042
Zytron1228 <dastelljes@outlook.com>
2024-06-19 19:10:57 -0500

    fixed #4*****

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,17 +15,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
-	char *new_name, *new_owner;
+	char *new_name = name, *new_owner = owner;
 
-	new_name = malloc(sizeof(name) + 1);
-	new_owner = malloc(sizeof(owner) + 1);
 	my_dog = malloc(sizeof(dog_t));
 
-	if (new_name == NULL || new_owner == NULL || my_dog == NULL)
+	if ( my_dog == NULL)
 		return (NULL);
-
-	new_name = strcpy(new_name, name);
-	new_owner = strcpy(new_owner, owner);
 
 	my_dog->name = new_name;
 	my_dog->age = age;
