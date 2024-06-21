28b25251c5941d30de61c3a3b65014070f9df707
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-20 22:07:17 -0500

    last project task 4 fix. Maybe?

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -22,16 +22,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; owner[i]; i++)
 		owner_len++;
 
-	new_name = malloc(name_len);
+	new_name = malloc(name_len + 1);
 	if (new_name == NULL)
 		return (NULL);
 
-	new_owner = malloc(owner_len);
+	for (i = 0; i <= name_len; i++)
+		new_name[i] = name[i];
+	new_name[i] = '\0';
+
+	new_owner = malloc(owner_len + 1);
 	if (new_owner == NULL)
 	{
 		free(new_name);
 		return (NULL);
 	}
+
+	for (i = 0; i <= owner_len; i++)
+		new_owner[i] = owner[i];
+	new_owner[i] = '\0';
 
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
@@ -40,14 +48,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_owner);
 		return (NULL);
 	}
-
-	for (i = 0; i <= name_len; i++)
-		new_name[i] = name[i];
-	new_name[i] = '\0';
-
-	for (i = 0; i <= owner_len; i++)
-		new_owner[i] = owner[i];
-	new_owner[i] = '\0';
 
 	my_dog->name = new_name;
 	my_dog->age = age;
