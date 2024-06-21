b5ba8991b967f0227ee864d7628db005d7759ad9
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-20 22:15:49 -0500

    directly malloc'd my_dog->name and my_dog->owner and then set them to the malloc'd copies of name and owner

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -26,20 +26,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_name == NULL)
 		return (NULL);
 
-	for (i = 0; i <= name_len; i++)
-		new_name[i] = name[i];
-	new_name[i] = '\0';
-
 	new_owner = malloc(owner_len + 1);
 	if (new_owner == NULL)
 	{
 		free(new_name);
 		return (NULL);
 	}
-
-	for (i = 0; i <= owner_len; i++)
-		new_owner[i] = owner[i];
-	new_owner[i] = '\0';
 
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
@@ -48,6 +40,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_owner);
 		return (NULL);
 	}
+
+	for (i = 0; i <= name_len; i++)
+		new_name[i] = name[i];
+	new_name[i] = '\0';
+
+	for (i = 0; i <= owner_len; i++)
+		new_owner[i] = owner[i];
+	new_owner[i] = '\0';
+
+	my_dog->name = malloc(name_len + 1);
+	my_dog->owner = malloc(owner_len + 1);
 
 	my_dog->name = new_name;
 	my_dog->age = age;
