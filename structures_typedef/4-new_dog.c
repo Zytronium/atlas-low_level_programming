eabbca5e7d5fd8f92fbb3836ec294e4684d49098
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-21 17:43:32 -0500

    test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -21,18 +21,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (j = 0; owner[j]; j++)
 		owner_len++;
 
-	my_dog = malloc((name_len + 1) * 2 + owner_len + sizeof(float));
+	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
 
-	my_dog->name = malloc((name_len + 1) * 2);
+	my_dog->name = malloc(sizeof(char) * name_len + 1);
 	if (my_dog->name == NULL)
 	{
 		free(my_dog);
 		return (NULL);
 	}
 
-	my_dog->owner = malloc((owner_len + 1) * 2);
+	my_dog->owner = malloc(sizeof(char) * owner_len + 1);
 	if (my_dog->owner == NULL)
 	{
 		free(my_dog->name);
