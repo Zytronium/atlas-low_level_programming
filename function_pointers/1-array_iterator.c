ae9e21bdf5dc99e3f10802eda2da4a3abdac1b77
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-20 23:28:35 -0500

    betty >:(

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,7 +2,8 @@
 #include <stddef.h>
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array.
+ * array_iterator - executes a function given as a parameter on each
+ * element of an array.
  *
  * @array: array of things to execute a function on
  * @size: size of the array
