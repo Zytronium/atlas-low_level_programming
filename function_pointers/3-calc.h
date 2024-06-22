d65f8d20308f66a59951afff9a5444417ef79de2
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-22 17:11:36 -0500

    test

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -13,14 +13,4 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int main(int argc, char *argv[]);
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-int (*get_op_func(char *s))(int, int);
-
 #endif
