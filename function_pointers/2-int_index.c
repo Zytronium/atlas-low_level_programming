b84b6b67f484eebd79b5909129ffe5448dc77f42
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-21 20:10:51 -0500

    fixed task2 by adding null check on array & cmp

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,14 +9,14 @@
  * @cmp: pointer to function that compares values
  *
  * Return: Index of the first element that cmp does
- * not return 0. Returns -1 if no element matches,
- * or if size is less than or equal to zero.
+ * not return 0. Returns -1 if there are no matches,
+ * if size is 0 or less, or if array or cmp are NULL.
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i <= size; i++)
