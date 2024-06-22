0c9b2c9063061cdecaead898486f3648980ae141
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-21 22:41:02 -0500

    started task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,9 +1,11 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * main - entry point | prints result
+ *
  * Return: Always 0
  */
 int main(int argc, char *argv[])
@@ -11,6 +13,18 @@ int main(int argc, char *argv[])
 	char operator = argv[3][0];
 	int num1 = atoi(argv[2]);
 	int num2 = atoi(argv[4]);
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strlen(argv[3]) != 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	return (0);
 }
