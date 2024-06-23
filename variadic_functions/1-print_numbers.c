31d766bb47a11aa7b9661eb4ac626e98126cba0e
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-23 14:27:01 -0500

    fix

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,18 +12,16 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	va_start(args, n);
 	while (i < n)
 	{
 		printf("%d", va_arg(args, int));
-		if (i != n - 1 && separator != NULL)
+		if (i != n && separator != NULL)
 			printf("%s", separator);
-		else
-			if (i == n - 1)
-				_putchar('\n');
 		i++;
 	}
+	printf("\n");
 	va_end(args);
 }
