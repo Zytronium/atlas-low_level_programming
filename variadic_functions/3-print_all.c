33f896b8777444c7aa9dbf486fec5b481714d167
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-23 16:02:29 -0500

    more progress maybe

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,22 +11,50 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0, n = strlen(format);
+	int i = 0, j = 0, n = strlen(format);
 
 	va_start(args, format);
 	while (i < n)
 	{
 		char f = format[i];
 
-		if (f == 'c' || f == 'i' || f == 'f' || f == 's')
-		{
-			if (va_arg(args, char *) == NULL)
-				printf("(nil)");
-			printf("%s",va_arg(args, char *));
+		switch (f) {
+			case 'c':
+				printf("%c", va_arg(args, char));
+				j++;
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				j++;
+				break;
+			case 'f':
+				printf("%f", va_arg(args, float));
+				j++;
+				break;
+			case 's':
+				if (va_arg(args, char *) == NULL)
+					printf("(nil)");
+				printf("%s", va_arg(args, char *));
+				j++;
+				break;
+			default:
+				break;
 		}
+
 		i++;
 	}
 	va_end(args);
 
 	printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("ceis", 'B', 3, "stSchool");
+	return (0);
 }
