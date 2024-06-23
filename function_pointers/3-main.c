fee36d8b6dd2b5914abd3c9ffb0f68a0f0a75785
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-06-22 19:23:38 -0500

    moved check for correct num of args to before variables are defined using argv

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,15 +13,15 @@ int main(int argc, char *argv[])
 	int num1;
 	int num2;
 
-	operator = argv[2];
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	operator = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	if (get_op_func(operator) == NULL)
 	{
