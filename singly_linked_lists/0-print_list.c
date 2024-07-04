ad38f78d2adadfdb1f49390d4130e7240dec7761
Zytron1228 <dastelljes@outlook.com>
2024-07-03 19:42:41 -0500

    almost finished task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,13 +1,21 @@
 #include "lists.h"
 
 /**
- * print_list - descr
+ * print_list - prints all the elements of a list_t list
  *
- * @h:
+ * @h: list to print
  *
- * Return: 0
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	return (0);
+	int i;
+	list_t *node = h;
+
+	for (i = 0; node != NULL; i++)
+	{
+		printf("[%d] %s\n", node->len, node->str != NULL ? node->str : "(nil)");
+		node = node->next;
+	}
+	return (i);
 }
