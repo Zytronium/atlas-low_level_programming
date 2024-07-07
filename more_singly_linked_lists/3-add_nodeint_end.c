388e914e5d1a4c2639fcc8aae4ee2e0feb7603ab
Zytron1228 <dastelljes@outlook.com>
2024-07-06 20:21:29 -0500

    did task 3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,18 +4,33 @@
 /**
  * add_nodeint_end - adds a new node to the end of a listint_t list
  *
- * @head: the pointer to the head of the list_t list
+ * @head: the pointer to the head of the listint_t list
  * @n: int to copy into the value of the new node
  *
  * Return: address of the new node, or NULL if it failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	return (*head);
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+		*head = newNode;
+	else
+		tail_node_int(*head)->next = newNode;
+
+	return (newNode);
 }
 
 /**
- * tail_node_int - finds the last (non-NULL) node of a list
+ * tail_node_int_int - finds the last (non-NULL) node of a list
  *
  * @head: the head or starting node of the search
  *
