5d16d328f503e9b34ee07145b0b723fe5092f2e7
Zytron1228 <dastelljes@outlook.com>
2024-07-08 13:45:27 -0500

    progress on bug fixing: no longer skipping 402, but stil lskipping 96

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -32,8 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	tempNode = newNode;
 
-	newNode->n = n;
-	newNode->next = newNode->next->next;
+	tempNode->n = n;
 	tempNode->next = newNode->next;
 
 	return (newNode);
