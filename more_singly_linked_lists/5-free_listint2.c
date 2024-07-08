f3830b930ea4ec1ca247c0afe552dc132846a844
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-07-07 20:30:22 -0500

    thing

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * free_listint2 - frees a listint_t list from memory
@@ -8,6 +9,13 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *nxt;
+
 	if (*head != NULL)
+	{
+		nxt = head[0]->next;
+		free(head);
+		free_listint(nxt);
 		head = NULL;
+	}
 }
