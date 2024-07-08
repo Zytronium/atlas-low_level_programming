5cf374ec283d754a8cc9fee55688499c062d49d6
Zytron1228 <dastelljes@outlook.com>
2024-07-08 16:22:03 -0500

    actually for real f i x e d

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 #include <malloc.h>
+#include <stdlib.h>
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given index.
@@ -14,23 +15,82 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *currentNode = *head, *tempNode;
-	unsigned int i = 0;
-
+	unsigned int i = 1;
+	
 	tempNode = malloc(sizeof(listint_t));
+
+	if (tempNode == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		*tempNode = _add_nodeint(head, n);
+		return (tempNode);
+	}
+		
 
 	while (i <= idx)
 	{
-		if (i < idx - 1)
+		if (i < idx)
 			currentNode = currentNode->next;
 		
-		if (i == idx - 1)
+		if (i == idx)
 		{
 			tempNode->n = n;
 			tempNode->next = currentNode->next;
 			currentNode->next = tempNode;
 		}
+		
 		i++;
 	}
 
 	return (tempNode);
+}
+
+/**
+ * add_nodeint - adds a new node to the beginning of a listint_t list
+ *
+ * @head: the pointer to the head of the listint_t list
+ * @n: integer to copy into the value of the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+listint_t _add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode, *temp;
+	int headIsNull = *head == NULL;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		exit(-1);
+
+	if (!headIsNull)
+	{
+		temp->n = head[0]->n;
+		temp->next = head[0]->next;
+	}
+	else
+	{
+		temp->n = 0;
+		temp->next = NULL;
+	}
+
+	newNode = malloc(sizeof(listint_t));
+
+	if (newNode == NULL)
+	{
+		free(temp);
+		exit(-1);
+	}
+
+	newNode->n = n;
+	newNode->next = temp;
+
+	if (headIsNull)
+		*head = malloc(sizeof(listint_t));
+
+	head[0]->n = newNode->n;
+	head[0]->next = !headIsNull ? newNode->next : NULL;
+
+	return (*newNode);
 }
