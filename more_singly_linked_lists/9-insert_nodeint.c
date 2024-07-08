d335ea6fe094cc324162d39656575c02c5286b08
Zytron1228 <dastelljes@outlook.com>
2024-07-08 13:18:04 -0500

    removed 2nd empty line

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -31,6 +31,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     newNode->next = newNode->next->next;
     tempNode->next = newNode->next;
 
-
 	return (newNode);
 }
