c960f44a4cfe6af50e11bf6be9178c7c373e54ae
Zytron1228 <dastelljes@outlook.com>
2024-07-08 17:06:16 -0500

    fixed memory leak

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *currentNode = *head;
+	listint_t *currentNode = *head, *temp;
 	unsigned int i = 1;
 
 	if  (*head == NULL || (index != 0 && head[0]->next == NULL))
@@ -20,7 +20,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
+		temp = head[0];
 		*head = head[0]->next;
+
+		free(temp);
 	}
 
 	while (i <= index)
@@ -33,62 +36,20 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 		if (i == index)
 		{
-		if (currentNode->next->next == NULL)
-			return (-1);
+			if (currentNode->next->next == NULL)
+				return (-1);
 
-		currentNode->next = currentNode->next->next;
+			temp = currentNode->next;
+			currentNode->next = currentNode->next->next;
+			
+			free(temp);
+
+			if (currentNode == NULL)
+				return (-1);
 		}
 
 		i++;
 	}
 
 	return (1);
-}
-
-/**
- * add_nodeint - adds a new node to the beginning of a listint_t list
- *
- * @head: the pointer to the head of the listint_t list
- * @n: integer to copy into the value of the new node
- *
- * Return: address of the new node, or NULL if it failed
- */
-listint_t _add_nodeint(listint_t **head, const int n)
-{
-	listint_t *newNode, *temp;
-	int headIsNull = *head == NULL;
-
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-		exit(-1);
-
-	if (!headIsNull)
-	{
-		temp->n = head[0]->n;
-		temp->next = head[0]->next;
-	}
-	else
-	{
-		temp->n = 0;
-		temp->next = NULL;
-	}
-
-	newNode = malloc(sizeof(listint_t));
-
-	if (newNode == NULL)
-	{
-		free(temp);
-		exit(-1);
-	}
-
-	newNode->n = n;
-	newNode->next = temp;
-
-	if (headIsNull)
-		*head = malloc(sizeof(listint_t));
-
-	head[0]->n = newNode->n;
-	head[0]->next = !headIsNull ? newNode->next : NULL;
-
-	return (*newNode);
 }
