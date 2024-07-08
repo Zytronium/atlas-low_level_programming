65653eb6e40c65a328ef55b3daba263634d8fca5
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-07-07 22:22:14 -0500

    betty

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,13 +1,21 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * pop_listint - deletes the head of a list, sets the next node to the head,
+ * and returns the data of the deleted node
+ *
+ * @param head: the head of the list
+ *
+ * Return: data of the deleted node
+ */
 int pop_listint(listint_t **head)
 {
-    int data = head[0]->n;
-    listint_t *new_head = head[0]->next;
+	int data = head[0]->n;
+	listint_t *new_head = head[0]->next;
 
-    free(*head);
-    *head = new_head;
-    
-    return (data);
+	free(*head);
+	*head = new_head;
+
+	return (data);
 }
