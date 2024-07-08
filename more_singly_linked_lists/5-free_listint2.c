878b2838bc2a3da2ea1e403fe4f660e3a2abdb88
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-07-07 20:26:54 -0500

    started task 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 
 /**
- * free_listint2 - descr
+ * free_listint2 - frees a listint_t list from memory
+ *
  * @head: the first node of listint_t list
  */
 void free_listint2(listint_t **head)
 {
-
+	if (*head != NULL)
+		head = NULL;
 }
