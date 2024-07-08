ba71cf613e7f7e4b85b6b73782e7205cfbb1679a
Zytron1228 <dastelljes@outlook.com>
2024-07-08 14:22:25 -0500

    created file for task 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index -deletes the node at
+ * the given index of a listint_t linked list.
+ *
+ * @head: the head of the listin_t linked list
+ * @index: the index of the node to be deleted
+ *
+ * Return: 1 on sucess; -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	
+	return (1);
+}
