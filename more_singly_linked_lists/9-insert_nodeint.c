acb424dbf794cefaf2688a450c2635c2207821a8
Zytron1228 <dastelljes@outlook.com>
2024-07-08 13:20:48 -0500

    malloc'ed tempNode

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -25,6 +25,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
     }
     
+    tempNode = malloc(sizeof(listint_t));
+
+    if (tempNode == NULL)
+        return (NULL);
+
     tempNode = newNode;
 
     newNode->n = n;
