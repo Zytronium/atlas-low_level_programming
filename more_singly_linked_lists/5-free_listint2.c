f1156b64cdfd7eb8485e7c25d57c80415fea9abb
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-07-07 21:00:17 -0500

    fixed task 5 maybe

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,16 @@
  */
 void free_listint2(listint_t **head)
 {
+	if (head == NULL)
+	{
+		free(NULL);
+		exit(0);
+	}
+
 	if (*head != NULL)
 	{
 		free_listint2(&head[0]->next);
 		free(*head);
+		*head = NULL;
 	}
-	*head = NULL;
 }
