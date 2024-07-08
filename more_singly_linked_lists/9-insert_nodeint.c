923b1a8b1a6a9339344372929b7920bc5f775212
Zytron1228 <dastelljes@outlook.com>
2024-07-08 13:25:37 -0500

    Fixed not adding nodes at beginning of list... again

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode = *head, *tempNode;
 	unsigned int i = 0;
 
-	while (i < idx)
+	while (i < idx || (idx == 0 && i == 0))
 	{
 		if (newNode == NULL || newNode->next == NULL)
 			return (NULL);
