ad8b5b58d5dc1b8890f0d54687cbfe9da42b6db5
Zytron1228 <dastelljes@outlook.com>
2024-07-08 17:22:23 -0500

    betty

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -41,7 +41,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 			temp = currentNode->next;
 			currentNode->next = currentNode->next->next;
-			
+
 			free(temp);
 
 			if (currentNode == NULL)
