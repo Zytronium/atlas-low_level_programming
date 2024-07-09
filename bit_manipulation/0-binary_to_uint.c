e258c1c05b5781d555810e12350558cb2468b9bf
Zytron1228 <dastelljes@outlook.com>
2024-07-08 19:24:56 -0500

    did task 0. I copy-pasted a lot of my own old code for this.

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * @b: string of chars assumed to be 0's and 1's
+ *
+ * Return: the converted number, or NULL if
+ * b is NULL or a char is not a 1 or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0, size = 0;
+	int i;
+	char bit;
+
+	while (b[size] != '\0')
+		size++;
+
+	for (i = (int) size - 1; i >= 0; --i)
+	{
+		if (b[i] == '0')
+			bit = 0;
+		else if (b[i] == '1')
+			bit = 1;
+		else
+			return (0);
+
+		result += bit * xPowY(2, ((i - (int) size) * - 1) - 1);
+	}
+	return (result);
+}
+
+/**
+ * xPowY - exponent of to the power of y
+ *
+ * @x: x
+ * @y: y
+ *
+ * Return: x to the power of y
+ */
+int xPowY(int x, int y)
+{
+	int i, output = 1;
+
+	for (i = 0; i < y; i++)
+		output *= x;
+
+	return (output);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
+int xPowY(int x, int y);
 
 #endif /* MAIN_H */
