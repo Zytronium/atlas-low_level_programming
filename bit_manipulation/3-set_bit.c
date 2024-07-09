d9ba1547f7c76be80e04d13b2b013207e47eecb4
Zytron1228 <dastelljes@outlook.com>
2024-07-08 22:19:35 -0500

    did task 3 and maybe it even works maybe betty won't have more than 1 error too

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a bit in n at index of index.
+ *
+ * @n: pointer to number to perform bit manipulation on
+ * @index: index of bit to manipulate
+ *
+ * Return: 1 on sucess; or -1 if failed
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	*n |= 1UL << index;
+
+	return (1);
+}
