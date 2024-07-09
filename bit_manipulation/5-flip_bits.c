54b0877367d91f8bc75701c5720cf04c47c5ecb9
Zytron1228 <dastelljes@outlook.com>
2024-07-08 22:33:33 -0500

    did task 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,10 +1,18 @@
 	#include "main.h"
 /**
- * main - descr.
+ * flip_bits - returns the number of bits you would need to flip to get from one number to another.
  *
- * Return: 0
+ * @n: first numb
+ * @m: second numb
+ * Return: numb of bits needed to flip to get one numb from the other
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (0);
+	unsigned int count = 0, i;
+
+	for (i = 0; n ^ m; i++)
+	{
+		(count += (n ^ m) >> (unsigned int) i) & 1;
+	}
+	return (count);
 }
