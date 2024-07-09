56934b85344edaee02b12426b79114b94c5f8e6f
Zytron1228 <dastelljes@outlook.com>
2024-07-08 20:51:37 -0500

    commit msg idk im lazy

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_binary - prints a number in binary
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int max = 1;
+
+	while ((unsigned long int) max < n)
+	{
+		max <<= 1;
+		if ((unsigned long int) max >= n)
+			break;
+		/*i++;*/
+	}
+
+	while ((unsigned long int) max > 1)
+	{
+		if ((unsigned long int) max >= n)
+		{
+		
+			printf("1");
+		}
+		else
+			printf("0");
+
+		n -= max;
+		max >>= 1;
+	}
+}
+
+/**
+ * xPowY - exponent of to the power of y
+ *
+ * @x: x
+ * @y: y
+ *
+ * Return: x to the power of y
+ */
+int xPowY(int x, int y)
+{
+	int i, output = 1;
+
+	for (i = 0; i < y; i++)
+		output *= x;
+
+	return (output);
+}
