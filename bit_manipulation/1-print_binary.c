e9256ebface9cfaf342fbfc12dbdd4eb1df4a44b
Zytron1228 <dastelljes@outlook.com>
2024-07-08 21:14:37 -0500

    moved n-= max into if statement

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,45 +8,25 @@
  */
 void print_binary(unsigned long int n)
 {
-	int max = 1;
+	unsigned long int max = 1;
 
-	while ((unsigned long int) max < n)
+	while (max < n)
 	{
 		max <<= 1;
-		if ((unsigned long int) max >= n)
+		if (max >= n)
 			break;
-		/*i++;*/
 	}
 
-	while ((unsigned long int) max > 0)
+	while (max > 0)
 	{
-		if ((unsigned long int) max <= n)
+		if (max <= n)
 		{
-		
+			n -= max;
 			printf("1");
 		}
 		else
 			printf("0");
 
-		n -= max;
 		max >>= 1;
 	}
-}
-
-/**
- * xPowY - exponent of to the power of y
- *
- * @x: x
- * @y: y
- *
- * Return: x to the power of y
- */
-int xPowY(int x, int y)
-{
-	int i, output = 1;
-
-	for (i = 0; i < y; i++)
-		output *= x;
-
-	return (output);
 }
