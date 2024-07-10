4acb36b4c5d6e7c5f90146553a7b5bfa0d20ed6b
Zytron1228 <dastelljes@outlook.com>
2024-07-10 18:26:09 -0500

    defined a macro for the original head

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stdlib.h>
-
+#ifndef oldHead /*it's just a little less confusing this way*/
+#define oldHead head
+/*
+ * now I can refer to the original head with this after moving it to
+ * the second item in the linked list, and I won't get confused about
+ * which head I'm referring to.
+ */
+#endif
 /**
  * add_dnodeint - adds a new node at the head a doubly linked list.
  *
@@ -11,18 +18,17 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *oldHead = malloc(sizeof(dlistint_t)); /* malloc oldHead */
+	dlistint_t *newHead = malloc(sizeof(dlistint_t)); /* malloc newHead */
 
 	/* again, I am assuming that h is the real head of the list */
-	if (*head == NULL || oldHead == NULL) /* head null or malloc fail check */
+	if (*head == NULL || newHead == NULL) /* head null or malloc fail check */
 		return (NULL);
 
 	/*create a new node and swap it with head (I got seg fault otherwise)*/
-	*oldHead = **head; /* clone head node into oldHead. */
-	head[0]->n = n; /* set head's data to the given data (n) */
-	head[0]->next = oldHead; /* set head's next ptr to oldHead */
-	head[0]->prev = NULL; /* set head's prev ptr to null */
-	oldHead->prev = *head; /* make sure old head prev ptr is new head */
+	newHead->n = n; /* set new head's data to the given data (n) */
+	newHead->next = *oldHead; /* set new head's next ptr to old head */
+	newHead->prev = NULL; /*set new head's prev ptr to null to make it the head*/
+	oldHead[0]->prev = newHead; /* make sure newHead prev ptr is old head */
 
-	return (*head); /*return ptr to new head*/
+	return (newHead); /*return ptr to new head*/
 }
