b674e5c8082256b4bec27ad8dcf8d7fa08175736
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2024-07-09 23:47:37 -0500

    betty?

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * create_file - creates a file.
  *
  * @filename: the name of the file to create
- * @text_content: content to write into the newly created file, null terminated
+ * @text_content: content to write into newly created file, null terminated
  *
  * Return: 1 on success, or -1 on failure
  * (file can not be created, file can not be written, write “fails”, etc…)
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL &&
-	write(fileDesc, text_content, strlen(text_content)) == -1)
+		write(fileDesc, text_content, strlen(text_content)) == -1)
 	{
 		close(fileDesc);
 		return (-1);
@@ -29,10 +29,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-/*
- * The created file must have these permissions: rw-------.
- * If the file already exists, do not change the permissions.
- * if the file already exists, truncate it
- * if filename is NULL return -1
- * if text_content is NULL create an empty file
-*/
