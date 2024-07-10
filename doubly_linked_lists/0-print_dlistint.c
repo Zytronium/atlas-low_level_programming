4c22c5e6a50b0f074e0d80276eb2a381136208c0
Zytron1228 <dastelljes@outlook.com>
2024-07-10 16:47:25 -0500

    betty (& and added the only file I actually manually added before previous commit)

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -12,9 +12,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int nodeCount = 0; /*number of nodes found and printed*/
 
-	/* I'm gonna assume that h is in fact the head, so I won't check if h.prev
+	/*
+	 * I'm gonna assume that h is in fact the head, so I won't check if h.prev
 	 * is NULL or not. However, if it's not NULL, we could iterate backwards
-	 * until we find NULL, and then start iterating up again. */
+	 * until we find NULL, and then start iterating up again.
+	 */
 	while (h != NULL) /*iterate until the tail, which is null*/
 	{
 		printf("%d\n", h->n); /*print the element's number*/
