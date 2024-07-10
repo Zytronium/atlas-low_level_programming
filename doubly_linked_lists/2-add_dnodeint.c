c0d13287e63784a70ead2054878841720fb602aa
Zytron1228 <dastelljes@outlook.com>
2024-07-10 18:11:38 -0500

    dereferenced tempNode and *head when cloning head into tempNode

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,11 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (tempNode == NULL || *head == NULL) /* malloc fail check */
 		return (NULL);
 
-	/*create new node and swap it with head (I got seg fault otherwise)*/
-	tempNode = *head; /* clone head node into tempNode. */
-	head[0]->n = n; /*set head's data to the given data (n)*/
-	head[0]->next = tempNode; /*set head's next ptr to tempNode*/
-	head[0]->prev = NULL; /*set head's prev ptr to null*/
+	/*create a new node and swap it with head (I got seg fault otherwise)*/
+	*tempNode = **head; /* clone head node into tempNode. */
+	head[0]->n = n; /* set head's data to the given data (n) */
+	head[0]->next = tempNode; /* set head's next ptr to tempNode */
+	head[0]->prev = NULL; /* set head's prev ptr to null */
 	tempNode->prev = *head; /*make sure old head (tempNode) prev ptr is new head*/
 
 	return (*head); /*return ptr to new head*/
