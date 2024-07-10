3cbb1621c0fdd7330cf84526cb59817cda1c06dc
Zytron1228 <dastelljes@outlook.com>
2024-07-10 18:30:46 -0500

    documentation

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,8 @@
 #endif
 /**
  * add_dnodeint - adds a new node at the head a doubly linked list.
+ * This assumes that head is correctly set as the true head of the list.
+ * It technically just adds a new node right before the node provided, "head."
  *
  * @head: head node of the doubly linked list
  * @n: number/data to be assigned to the new node.
@@ -20,14 +22,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newHead = malloc(sizeof(dlistint_t)); /* malloc newHead */
 
-	/* again, I am assuming that h is the real head of the list */
 	if (*head == NULL || newHead == NULL) /* head null or malloc fail check */
 		return (NULL);
 
 	/*create a new node and swap it with head (I got seg fault otherwise)*/
 	newHead->n = n; /* set new head's data to the given data (n) */
 	newHead->next = *oldHead; /* set new head's next ptr to old head */
-	newHead->prev = NULL; /*set new head's prev ptr to null to make it the head*/
+	newHead->prev = NULL; /* set new head's prev ptr to null to make it the head */
 	oldHead[0]->prev = newHead; /* make sure newHead prev ptr is old head */
 
 	return (newHead); /*return ptr to new head*/
