8ddf76345713dc577f0f6be5aaef4e2efd6883ba
Zytron1228 <dastelljes@outlook.com>
2024-07-13 20:58:00 -0500

    mallocs head if its null, swaps head with new head by setting *head to point to newHead, and sets head's prev ptr to point to newHead (which at this point is the old head, and head is the new head)

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -3,6 +3,8 @@
 
 /**
  * add_dnodeint - adds a new node at the head a doubly linked list.
+ * This assumes that head is correctly set as the true head of the list.
+ * It technically just adds a new node right before the node provided, "head."
  *
  * @head: head node of the doubly linked list
  * @n: number/data to be assigned to the new node.
@@ -13,25 +15,27 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newHead = malloc(sizeof(dlistint_t)); /* malloc newHead */
 
-	/* again, I am assuming that h is the real head of the list */
 	if (newHead == NULL) /* malloc fail check */
-		return (NULL); /*return null to indicate failure*/
+		return (NULL); /* return null to indicate failure */
 
-	/*create new node at behind the current head */
-	newHead->n = n; /*set newHead's data to the given data (n)*/
+	/* create new node at behind the current head */
+	newHead->n = n; /* set newHead's data to the given data (n) */
 	newHead->next = *head; /*make newHead the actual new head & continue chain*/
-	newHead->prev = NULL; /*make newHead the actual new head*/
-	if (*head == NULL)
+	newHead->prev = NULL; /* make newHead the actual new head */
+
+	if (*head == NULL) /* malloc head if it doesn't exist */
 	{
 		*head = malloc(sizeof(dlistint_t)); /* create head node if its null */
 		if (*head == NULL) /* malloc fail check */
 		{
-			free(newHead); /*free all memory*/
-			return (NULL); /*what does this look like it does*/
+			free(newHead); /* free all memory */
+			return (NULL); /* what does this look like it does */
 		}
 	}
-	**head = *newHead; /*swap newHead with head*/
-	free(newHead); /*free newHead*/
+
+	*head = newHead; /*swap newHead with head*/
+	/* Beyond here, newHead is the OLD head, and *head is the NEW head */
+	head[0]->prev = newHead; /*set the head's prev ptr*/
 
 	return (*head); /*return ptr to new head*/
 }
