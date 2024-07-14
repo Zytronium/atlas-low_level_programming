e9f3a6533302234c85f96e117fdf8bb639423745
Zytron1228 <dastelljes@outlook.com>
2024-07-14 18:04:00 -0500

    task 4 setup

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees an entire dlistint_t doubly linked list.
+ * Assumed that the head provided is the actual head.
+ * I won't program it to iterate down the list until it finds
+ * the real head unless the checker doesn't link it.
+ *
+ * @head: the head of the doubly linked list to free
+ */
+void free_dlistint(dlistint_t *head)
+{
+	free(betty); /* please free me from betty*/
+}
