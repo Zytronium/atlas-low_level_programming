e09d405b0ba87682caff2e5bdfdd30374215db7f
Zytron1228 <dastelljes@outlook.com>
2024-07-13 20:17:48 -0500

    removed oldHead macro to avoid confusion, sacrificed a ton of readability to the gods of Betty, added function-macros to reduce lines below 40

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,12 +1,18 @@
 #include "lists.h"
-#include <stdio.h>
-#ifndef oldHead /*it's just a little less confusing this way*/
-#define oldHead head
-/*
- * now I can refer to the original head with this after moving it to
- * the second item in the linked list, and I won't get confused about
- * which head I'm referring to.
- */
+
+#ifndef safeMallocList
+#define safeMallocList(what) what = malloc(sizeof(dlistint_t)); \
+if (what == NULL)                                               \
+{                                                               \
+free(newHead);                                                  \
+return (NULL);                                                  \
+}
+#endif
+
+#ifndef freeAndReturn
+#define freeAndReturn(fr, ret) free(fr); /*delete temp helper node*/ \
+return (ret)/*return ptr to new head*/
+
 #endif
 
 /**
@@ -21,94 +27,41 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *newHead = malloc(sizeof(dlistint_t)); /* create newHead */
+	dlistint_t *newHead = malloc(sizeof(dlistint_t)), *tempNode; /* create newHead */
 	int headIsNull = *head == NULL;
 
 	if (newHead == NULL) /* malloc fail check */
 		return (NULL);
-
 	newHead->n = n; /* set new head's data to the given data (n) */
 	newHead->prev = NULL; /* set newHead's prev ptr to NULL */
 	newHead->next = NULL; /* set newHead's next ptr to NULL */
-
-	if (headIsNull) /* if old head is NULL, malloc it */
+	if (headIsNull) /* if head is NULL, malloc it */
 	{
-		*oldHead = malloc(sizeof(dlistint_t)); /* malloc old head */
-		if (*oldHead == NULL) /* malloc fail check */
-		{
-			free(newHead); /* free everything */
-			return (NULL); /* what does this look like it does? */
-		}
-
-		/* Note: oldHead is a macro for head. */
+		safeMallocList(*head)/*don't need ; because its a macro ending with }*/
 		**head = *newHead; /* clone the new node into the head */
-		free(newHead); /* free new node, as it's no longer gonna be used */
-		return (*head); /* return newly created head node */
-	} /* ^ this is my bug cause most likely */
-	else
-	{
-		dlistint_t *tempNode = malloc(sizeof(dlistint_t)); /*create temp node*/
-		if (tempNode == NULL) /* malloc fail check */
-		{
-			free(newHead); /* free all memory if malloc failed */
-			return (NULL); /* return NULL to indicate failure */
-		}
-		if (oldHead[0]->next == NULL) /*this would only ever be the 2nd node*/
-		{
-			oldHead[0]->prev = newHead; /*move oldHead ahead of newHead*/
-			newHead->next = *oldHead; /*move newHead behind oldHead*/
-
-			*tempNode = **oldHead; /*clone oldHead properties into tempNode*/
-			**head = *newHead; /*make newHead the head*/
-			/*Note: oldHead is a macro for head*/
-			*newHead = *tempNode; /*clone oldHead's og properties into newHead*/
-
-			/*BEYOND HERE, newHead IS THE OLD HEAD*/
-			free(tempNode); /*delete temp helper node*/
-
-			head[0]->next = newHead; /*set new head.nxt to point to next item*/
-			/*otherwise, it points to itself. I know because I whiteboarded it*/
-			newHead->prev = *head; /*same thing again but reverse*/
-		}
-		else
-		{
-			/* Clone head into temp */
-			*tempNode = **head;
-			/* Clone newHead into head */
-			**head = *newHead;
-			/* Clone *temp->next (old head->next) into newHead */
-			*newHead = *tempNode->next;
-			/* Clone temp into *temp->next */
-			*tempNode->next = *tempNode;
-
-			/* Set head->next to pnt to temp->next */
-			oldHead[0]->next = tempNode->next;
-			/* Set head->next->next to pnt to newHead */
-			oldHead[0]->next->next = newHead;
-			/* Set newHead->prev to pnt to head->next(->next?) */
-			newHead->prev = head[0]->next->next;
-			/* Set head->next->prev to pnt to head */
-			head[0]->next->prev = *head;
-
-			/*Trust me, it made sense while whiteboarding it. Too hard to explain*/
-			/* why doesn't it work */
-		}
-
+		freeAndReturn (newHead, *head);/*free unused node,& return ptr to head*/
 	}
-
-	return (newHead); /*return ptr to new head*/
+	safeMallocList(tempNode) /*create temp node*/
+	if (head[0]->next == NULL) /*this would only ever be the 2nd node*/
+	{
+		head[0]->prev = newHead; /*move head ahead of newHead*/
+		newHead->next = *head; /*move newHead behind head*/
+		*tempNode = **head; /*clone head properties into tempNode*/
+		**head = *newHead; /*make newHead the new head*/
+		*newHead = *tempNode; /*clone head's og properties into newHead*/
+		/*BEYOND HERE, newHead IS THE OLD HEAD*/
+		head[0]->next = newHead;/*set new head.nxt to point to next item*/
+		newHead->prev = *head; /*same thing again but reverse*/
+		freeAndReturn(tempNode, *head);
+	} /*else: */
+	*tempNode = **head;/*Clone head into temp*/
+	**head = *newHead;/*Clone newHead into head*/
+	*newHead = *tempNode->next;/*Clone *temp->next into newHead*/
+	*tempNode->next = *tempNode;/*Clone temp into *temp->next*/
+	head[0]->next = tempNode->next;/*Set head->next to pnt to temp->next*/
+	head[0]->next->next = newHead;/*Set head->next->next to pnt to newHead*/
+	newHead->prev = head[0]->next->next;/*Set newHead->prev to pnt to head->next(->next?)*/
+	head[0]->next->prev = *head;/*Set head->next->prev to pnt to head*/
+	freeAndReturn(tempNode, *head);
 }
-
-/*
- * Note: I just had a moment of clarity. I think know what's happening now!
- * The reason it starts at 0 and ends with the last node when printing is
- * because when the head is not null, it sets the previous node to the new
- * node's value, overwriting it.
- *
- * Update: this is the exact same print list bug I've been debugging for
- * a couple days on a personal project... It starts printing at the item
- * right before the head, and in this case, stops right before the tail.
- *
- * Update2: Nevermind. It actually just starts at the tail and then prints
- * from the head after that like nothing happened, then crashes.
- */
+/* check my jpeg in this dir for white boarding that may help explain */
--- doubly_linked_lists/main.c
+++ doubly_linked_lists/main.c
@@ -11,6 +11,7 @@ int main(void)
 	dlistint_t *head;
 
 	head = NULL;
+	add_dnodeint(&head, 0);
 	add_dnodeint(&head, 1);
 	add_dnodeint(&head, 2);
 	add_dnodeint(&head, 3);
