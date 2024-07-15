c0ddb8b2393467e15e2e1bc2a25b22e3f53f72c9
Zytron1228 <dastelljes@outlook.com>
2024-07-15 11:39:32 -0500

    checker test task 2

--- doubly_linked_lists/main.c
+++ doubly_linked_lists/main.c
@@ -11,11 +11,14 @@ int main(void)
 	dlistint_t *head;
 
 	head = NULL;
+	add_dnodeint(&head, 0);
+	add_dnodeint(&head, 1);
+	add_dnodeint(&head, 2);
+	add_dnodeint(&head, 3);
+	add_dnodeint(&head, 4);
+	add_dnodeint(&head, 98);
+	add_dnodeint(&head, 402);
+	add_dnodeint(&head, 1024);
 	print_dlistint(head);
-	printf("-----------------\n");
-	insert_dnodeint_at_index(&head, 0, 4096);
-	print_dlistint(head);
-	free_dlistint(head);
-	head = NULL;
 	return (EXIT_SUCCESS);
 }
