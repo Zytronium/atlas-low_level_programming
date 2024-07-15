62d94fb2e3ac91b07ce3e56378b67bb88f1037d5
Zytron1228 <dastelljes@outlook.com>
2024-07-14 19:05:42 -0500

    pasted function from previous task and fixed its documentation

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - descr
+ * get_dnodeint_at_index - gets the node at the given index
  *
  * @head: the head of the doubly linked list
  * @index: index of the list to find the node at
--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,3 +15,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	return (newNode);
 }
+
+/**
+ * get_dnodeint_at_index - gets the node at the given index
+ *
+ * @head: the head of the doubly linked list
+ * @index: index of the list to find the node at
+ *
+ * Return: the node at the given index, or NULL if it doesn't exist.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *node = head;
+
+	while (i < index)
+	{
+		if (node == NULL)
+			return (NULL);
+
+		node = node->next;
+		i++;
+	}
+
+	return (node);
+}
