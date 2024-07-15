4048572b79aa8eeb17555b6f105066269fad1968
Zytron1228 <dastelljes@outlook.com>
2024-07-15 13:49:08 -0500

    finished task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,15 +2,31 @@
 #include <stdlib.h>
 
 /**
- * hash_table_create - creates a new hash_table_t hash table
+ * hash_table_create - creates a new empty hash_table_t hash table
  *
  * @size: size of the array
  *
- * Return: pointer to the newly created hash table
+ * Return: pointer to the newly created hash table, or NULL if failed
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *newHashTable;
+	hash_table_t *newHashTable = malloc(sizeof(hash_table_t));
 
+	/* malloc fail check */
+	if (newHashTable == NULL)
+		return (NULL);
+
+	/* init newHashTable */
+	newHashTable->size = size;
+	newHashTable->array = malloc(sizeof(hash_node_t **) * size);
+
+	/* malloc fail check */
+	if (newHashTable->array == NULL)
+	{
+		free(newHashTable);
+		return (NULL);
+	}
+
+	/* return pointer to new hash table if nothing fails */
 	return (newHashTable);
 }
