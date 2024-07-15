8324fd45be6257121168e2f6bef17c3c708b627f
Zytron1228 <dastelljes@outlook.com>
2024-07-15 14:59:42 -0500

    started task 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_set -  adds an element to the hash table
+ *
+ * @ht: hash table you want to add or update the key/value to
+ * @key: non-empty key string
+ * @value: value associated with the key
+ *
+ * Return: 1 on success, 0 if it fails
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
