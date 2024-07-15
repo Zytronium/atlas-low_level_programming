311387796dc54c9f2c8c0f84e93137d5fdb66eb9
Zytron1228 <dastelljes@outlook.com>
2024-07-15 18:12:10 -0500

    finished task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * hash_table_get - finds value associated with
@@ -13,5 +14,24 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	return (EXIT_SUCCESS);
+	unsigned long int index;
+	hash_node_t *check;
+
+	/* null check on hash table and key */
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	/* get the index associated with the given key */
+	index = key_index((const unsigned char *) key, ht->size);
+	/* iterate through the linked list at the right idx */
+	check = ht->array[index];
+	while (check != NULL)
+	{
+		/* return the value at this node if the key matches */
+		if ((strcmp(check->key, key) == 0))
+			return (check->value);
+		/* iterate to the next node in the list */
+		check = check->next;
+	}
+	/* if not found, return NULL */
+	return (NULL);
 }
