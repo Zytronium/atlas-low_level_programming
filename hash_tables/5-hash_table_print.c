f05cab565498b53419ee33f30cac584c1245290d
Zytron1228 <dastelljes@outlook.com>
2024-07-15 21:36:27 -0500

    added print statements

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * hash_table_print - Prints the contents of a hash table
@@ -9,24 +10,33 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *list, *node;
-	int i;
+	int i, lastElmnt = 0;
 
 	if (ht == NULL)
 		return;
 
 	list = ht->array[0];
-	node = list;
 
+	putchar('{');
 	/* iterate over ht->array */
-	for (i = 0; i <= ht->size && list != NULL; list = ht->array[i])
+	for (i = 0; i <= ht->size; list = ht->array[i])
 	{
+		node = list;
 		/* iterate over each linked list in the array */
 		while (node != NULL)
 		{
+			/*
+			 * TODO: add variable to determine if this s the last item
+			 *  in the array
+			 */
+
 			/* print item contents */
-			/*WIP*/
+			printf("'%s': '%s'%s", node->key, node->value,
+				   (!lastElmnt) ? ", " : ""); /* add commas where needed */
 
 			node = node->next;
 		}
+		i++;
 	}
+	printf("}\n");
 }
--- hash_tables/main.c
+++ hash_tables/main.c
@@ -22,42 +22,6 @@ int main(void)
 	hash_table_set(ht, "Asterix", "Obelix");
 	hash_table_set(ht, "obamna", "SODA!");
 	hash_table_set(ht, "hash", "Table.");
-	hash_table_set(ht, "Betty formatting", "nightmare");
-	hash_table_set(ht, "98", "Battery Street");
-	hash_table_set(ht, "Rick", "Never Gonna Give You Up!");
-	hash_table_set(ht, "main.c", "#include <stdio.h>\n"
-								 "#include <stdlib.h>\n"
-								 "#include \"hash_tables.h\"\n"
-								 "\n"
-								 "/**\n"
-								 " * main - checks the code\n"
-								 " *\n"
-								 " * Return: EXIT_SUCCESS (0)\n"
-								 " */\n"
-								 "int main(void)\n"
-								 "{\n"
-								 "\thash_table_t *ht;\n"
-								 "\n"
-								 "\tht = hash_table_create(1024);\n"
-								 "\thash_table_print(ht);\n"
-								 "\thash_table_set(ht, \"c\", \"fun\");\n"
-								 "\thash_table_set(ht, \"python\", \"awesome\");\n"
-								 "\thash_table_set(ht, \"Steve\", \"Pixel Guy\");\n"
-								 "\thash_table_set(ht, \"Bob\", \"and Kris love asm\");\n"
-								 "\thash_table_set(ht, \"Rick\", \"Pickle\");\n"
-								 "\thash_table_set(ht, \"N\", \"queens\");\n"
-								 "\thash_table_set(ht, \"Jack\", \"pumpkin lantern\");\n"
-								 "\thash_table_set(ht, \"Asterix\", \"Obelix\");\n"
-								 "\thash_table_set(ht, \"obamna\", \"SODA!\");\n"
-								 "\thash_table_set(ht, \"hash\", \"Table.\");\n"
-								 "\thash_table_set(ht, \"Betty formatting\", \"nightmare\");\n"
-								 "\thash_table_set(ht, \"98\", \"Battery Street\");\n"
-								 "\thash_table_set(ht, \"Rick\", \"Never Gonna Give You Up!\");\n"
-								 "\thash_table_set(ht, \"main.c\", \"C O D E C E P T I O N\\n\");\n"
-								 "\thash_table_print(ht);\n"
-								 "\n"
-								 "\treturn (EXIT_SUCCESS);\n"
-								 "}\n");
 	hash_table_print(ht);
 
 	return (EXIT_SUCCESS);
