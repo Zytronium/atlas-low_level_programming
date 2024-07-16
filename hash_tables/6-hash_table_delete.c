3ee504b3369310591adf81658a80bb668ba7e466
Zytron1228 <dastelljes@outlook.com>
2024-07-15 21:50:46 -0500

    started task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_t - descr
+ *
+ * @ht: hash table to delete
+ *
+ * Return: ☺☺☺you☻☻☻are☺☺☺an☻☻☻
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+
+}
