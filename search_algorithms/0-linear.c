28a367d8c73114e1238bb504b454e8f59ad933b5
zytronium <dastelljes@outlook.com>
2024-08-09 16:36:32 -0500

    almost finished task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,5 +13,22 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	int i = 0;
 
+	if (array == NULL)
+		return (-1); /* indicate failure */
+
+	while (i <= size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %d\n", value, i);
+			return (i); /* return index of value found */
+		}
+		i++;
+	}
+
+	printf("Could not find value %d\n", value);
+	return (-1); /* indicate value was not found */
 }
